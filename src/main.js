e90714f87120e2bdde22af6af56dc98daaeff571
Roger Hestad <roger@doublethink.no>
2021-03-03 21:09:00 +0100

    main: Refactors main to use separate lines for app.use, mount, etc

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ const i18n = createI18n({
   locale: "no",
 });
 
-createApp(App)
-  .use(router)
-  .use(i18n)
-  .mount("#app");
+const app = createApp(App);
+app.config.devtools = true;
+app.use(router)
+app.use(i18n)
+app.mount("#app");
+
