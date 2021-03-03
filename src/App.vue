9b12233357ee68b944d8e0403d3f65a4984615ad
Roger Hestad <roger@doublethink.no>
2021-03-03 16:14:04 +0100

    Refactors i18n message code to App
    Done because the router isn't ready in main

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,54 @@
   router-link(to="/about") About
   router-view
 </template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      languageInitiated: false,
+    };
+  },
+  watch: {
+    // * Since route params are not always ready, we need a watcher
+    $route() {
+      if (!this.languageInitiated && this.$route.params.lang)
+        this.initLanguage();
+    },
+  },
+  computed: {
+    isValidLang() {
+      const lang = this.$route.params.lang;
+      if (lang == "nn" || lang == "nb") return true;
 
+      return false;
+    },
+  },
+  methods: {
+    // * Inits i18n messages to the correct language
+    initLanguage() {
+      if (!this.isValidLang) {
+        this.$router.push("/nb");
+      } else {
+        let messages = {};
+        if (this.$route.params.lang == "nn") {
+          // * window.litteraturhistorieDictionaryNn/Nb is loaded by index.html
+          messages = window.litteraturhistorieDictionaryNn;
+        } else {
+          messages = window.litteraturhistorieDictionaryNb;
+        }
+
+        this.$i18n.setLocaleMessage("no", messages);
+        this.languageInitiated = true;
+      }
+    },
+  },
+  created() {
+    // * Init language if lang param is provided
+    if (this.$route.params.lang) this.initLanguage();
+  },
+};
+</script>
 <style lang="scss">
 @use "./styles/main";
 </style>
--- src/main.js
+++ src/main.js
@@ -3,20 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import { createI18n } from "vue-i18n";
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const langCode = urlParams.get("lang");
-
-let messages = {};
-if (langCode == "nn") {
-  messages.no = window.statistikkDictionaryNn;
-} else {
-  messages.no = window.statistikkDictionaryNb;
-}
-
 const i18n = createI18n({
   locale: "no",
-  messages
 });
 
 createApp(App)
