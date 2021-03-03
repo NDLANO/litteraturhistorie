90c2bbc67cf15309cb10fb550722cc033f31f5a5
Roger Hestad <roger@doublethink.no>
2021-03-03 14:40:55 +0100

    Adds TimeLine view component and sets it as default home component

---
+++ src/views/TimeLine.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>TimeLine</h1>
+    <p v-if="periodLoaded">Period loaded</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TimeLine",
+  data() {
+    return {
+      periodLoaded: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
