e9fe9e68ed865630e4fe2750c05fd625ce46b246
Roger Hestad <roger@doublethink.no>
2021-03-04 13:13:51 +0100

    router: Adds route to norron (Norrøn tid)

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,26 @@ const routes = [
       ),
     },
   },
+  {
+    path: "/nn/periods/norron/",
+    name: "NorronNN",
+    components: {
+      default: TimeLine,
+      books: import(
+        /* webpackChunkName: "beowulfnn" */ "../periods/nn/Norron.vue"
+      ),
+    },
+  },
+  {
+    path: "/nb/periods/norron/",
+    name: "NorronNB",
+    components: {
+      default: TimeLine,
+      books: import(
+        /* webpackChunkName: "beowulfnb" */ "../periods/nb/Norron.vue"
+      ),
+    },
+  },
 
   // {
   //   path: "/about",
