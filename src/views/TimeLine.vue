154ef98159387995db768221b361db1434d965f5
Roger Hestad <roger@doublethink.no>
2021-03-15 18:02:53 +0100

    TimeLine: Removes code loading data from books and periods json files

--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 #timeline 
   h1 TimeLine - {{ globalVars.langCode }}
-  div(v-if="periods")
+  div(v-if="periodRoutes")
     h2 Perioder
     ul
       li(v-for="period in periodRoutes", :key="period.name") {{ period.meta.from }} - {{ period.meta.to }} <router-link :to="period.path">{{ period.meta.title }}</router-link>
@@ -26,8 +26,6 @@ export default {
   data() {
     return {
       periodLoaded: false,
-      periods: null,
-      books: null,
       artists: null,
       artistRoutes: null,
       bookRoutes: null,
@@ -45,14 +43,6 @@ export default {
   // },
   async created() {
     const ucLangCode = this.globalVars.langCode.toUpperCase();
-
-    // * load periods
-    const periodsFileName = "config/periods" + ucLangCode + ".json";
-    this.periods = JSON.parse(readFile(periodsFileName));
-
-    // * load books
-    const booksFileName = "config/books" + ucLangCode + ".json";
-    this.books = JSON.parse(readFile(booksFileName));
 
     // * load artists
     const artistsFileName = "config/artists" + ucLangCode + ".json";
