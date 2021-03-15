2a418353bf43c66c58a0a2ae13184521522a6dad
Roger Hestad <roger@doublethink.no>
2021-03-15 22:09:02 +0100

    TimeLine: Comments out old HTML

--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -92,20 +92,20 @@
               )
           SeparatorAuthor
 //#timeline 
-h1 TimeLine - {{ globalVars.langCode }}
-div(v-if="periodRoutes")
-  h2 Perioder
-  ul
-    li(v-for="period in periodRoutes", :key="period.name") {{ period.meta.from }} - {{ period.meta.to }} <router-link :to="period.path">{{ period.meta.title }}</router-link>
+  h1 TimeLine - {{ globalVars.langCode }}
+  div(v-if="periodRoutes")
+    h2 Perioder
+    ul
+      li(v-for="period in periodRoutes", :key="period.name") {{ period.meta.from }} - {{ period.meta.to }} <router-link :to="period.path">{{ period.meta.title }}</router-link>
 
-  h2 Bøker
-  ul
-    li(v-for="book in bookRoutes", :key="book.meta.title") {{ book.meta.year }} - <router-link :to="book.path">{{ book.meta.title }}</router-link> av {{ book.meta.author }}
+    h2 Bøker
+    ul
+      li(v-for="book in bookRoutes", :key="book.meta.title") {{ book.meta.year }} - <router-link :to="book.path">{{ book.meta.title }}</router-link> av {{ book.meta.author }}
 
-  h2 Forfattere
-  ul
-    li(v-for="artist in artists", :key="artist.NAVN") {{ artist.FRA }} - {{ artist.TIL }} {{ artist.NAVN }}
-      div(v-html="artist.OMTALE")
+    h2 Forfattere
+    ul
+      li(v-for="artist in artists", :key="artist.NAVN") {{ artist.FRA }} - {{ artist.TIL }} {{ artist.NAVN }}
+        div(v-html="artist.OMTALE")
 </template>
 
 <script>
