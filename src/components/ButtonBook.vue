d9ab6c487aea94c841264af4676f0ff4c70de7b7
Roger Hestad <roger@doublethink.no>
2021-03-15 21:54:07 +0100

    Adds ButtonBook component with IconButtonBook
    ButtonBook is a button showing a book on the timeline

---
+++ src/components/ButtonBook.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+.btnBook(@click="showLink")
+  .btnBook_icon
+    IconButtonBook
+  .btnBook_infos
+    .btnBook_title {{ title }}
+    .btnBook_author {{ author }}
+</template>
+
+<script>
+import IconButtonBook from "@/components/ui/IconButtonBook";
+
+export default {
+  name: "ButtonBook",
+  components: {
+    IconButtonBook,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    author: {
+      type: String,
+      default: "",
+    },
+    path: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    showLink() {
+      if (this.path !== "") {
+        this.$router.push(this.path);
+      }
+    },
+  },
+};
+</script>
