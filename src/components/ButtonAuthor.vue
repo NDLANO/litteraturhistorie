97ed6cd23e258d00ff2a34f1f99fc3ffb6ba61e1
Roger Hestad <roger@doublethink.no>
2021-03-16 09:56:10 +0100

    Adds ButtonAuthor and IconPlus components
    ButtonAuthor automatically shows no-link version if no path is provided.
    
    The path prop may be replaced with the description at a later point

---
+++ src/components/ButtonAuthor.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+button.btnAuthor(:class="{ 'btnAuthor-noDescription': !gotDescription }")
+  div {{ name }}
+  IconPlus(v-if="path !== ''")
+</template>
+
+<script>
+import IconPlus from "@/components/ui/IconPlus";
+export default {
+  components: {
+    IconPlus,
+  },
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    path: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    gotDescription() {
+      console.log("got description = ", this.path !== "");
+      if (this.path !== "") return true;
+
+      return false;
+    },
+  },
+};
+</script>
+
