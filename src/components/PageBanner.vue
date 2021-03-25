0456d6ce4ccf96387d04b529e8fad02241ecc845
Roger Hestad <roger@doublethink.no>
2021-03-25 13:56:31 +0100

    Refactoring of Book page
    Moves everything but the content to PageBook. Content is inserted using slots

---
+++ src/components/PageBanner.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.page_banner
+  img(src="@/assets/media/img/banner3.jpg")
+</template>
+
+<script>
+export default {
+  name: "PageBanner",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/PageBook.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.lo_fullOverlay
+  .lo_fullContainer
+    .lo_pageTopBar
+      ButtonBackArrow
+      .pageTopBar_firstItem {{ this.title }}
+      pageTopBar_secondItem {{ this.author }}
+    
+    .page
+      PageBanner
+      .page_content
+        PageBookAuthor(:title="title" :author="author")
+        slot
+
+        button(@click="$router.go(-1)") {{ $t("general.pageBookBackButton")}}
+
+</template>
+<script>
+import PageBanner from "@/components/PageBanner";
+import PageBookAuthor from "@/components/PageBookAuthor";
+
+import ButtonBackArrow from "@/components/ButtonBackArrow";
+
+export default {
+  name: "PageBook",
+  components: {
+    PageBanner,
+    PageBookAuthor,
+    ButtonBackArrow,
+  },
+  computed: {
+    author() {
+      return this.$route.meta.author;
+    },
+    title() {
+      return this.$route.meta.title;
+    },
+    year() {
+      return this.$route.meta.year;
+    },
+    cover() {
+      return this.$route.meta.cover;
+    },
+  },
+};
+</script>
