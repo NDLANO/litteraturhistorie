13b03b0ca505770de296fff189e652f02cdb766b
Roger Hestad <roger@doublethink.no>
2021-03-29 10:35:07 +0200

    Renames NdlaVideo to NdlaPlayer
    Since the player will also play audio files

--- src/books/asbjornsenmoe/AsbjornsenMoeNN.vue
+++ src/books/asbjornsenmoe/AsbjornsenMoeNN.vue
@@ -4,7 +4,7 @@ PageBook
 
   p I 1814 får Noreg sjølvråderett og eiga grunnlov. Samtidig veks interessa for alt som kan kallast opphavleg norsk. Men Noreg har vore under Danmark i fleire hundre år, og i heile denne perioden har norske forfattarar skrive på dansk. Ved inngangen til 1800talet finst det derfor ingen norskspråkleg litteratur bortsett frå eventyr, viser og segner som var over­leverte munnleg frå generasjon til genera­sjon. 
 
-  NdlaVideo
+  NdlaPlayer(source="video/BigBuckBunny.mp4")
 
   h3 Større interesse for folkelitteraturen i Europa
 
@@ -101,12 +101,12 @@ PageBook
 
 <script>
 import PageBook from "@/components/PageBook";
-import NdlaVideo from "@/components/NdlaVideo";
+import NdlaPlayer from "@/components/NdlaPlayer";
 
 export default {
   name: "Book",
   components: {
-    NdlaVideo,
+    NdlaPlayer,
     PageBook,
   },
 };
---
+++ src/components/NdlaPlayer.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+video(ref="player" playsinline controls :data-poster="poster")
+  source(:src="source")
+</template>
+
+<script>
+import Plyr from "plyr";
+
+export default {
+  name: "NdlaPlayer",
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+    poster: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    console.log("ref player = ", this.$refs.player);
+
+    // * Init plyr
+    this.player = new Plyr(this.$refs.player);
+  },
+};
+</script>
