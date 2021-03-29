f4e3540588b74cf8f487616ffb9bceb32ef3cc94
Roger Hestad <roger@doublethink.no>
2021-03-29 14:49:35 +0200

    Adds PagePeriodCircle component
    Displays the circle and title in the middle of the period pages

---
+++ src/components/PagePeriodCircle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page_circleEra">
+    <img :src="src" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PagePeriodCircle",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+  created() {
+    console.log("PagePeriodCircle.created: src = ", this.src);
+  },
+};
+</script>
