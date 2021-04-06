c72130062cb34ef785de950477539cd88c6196fb
Roger Hestad <roger@doublethink.no>
2021-04-06 19:33:30 +0200

    TimeLine: Duplicates the era, adjusts width and adds comments

--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -21,13 +21,15 @@
           h1 Språk- og litteraturhistorisk tidslinje
           p Duis augue tortor, gravida non nisi ut, bibendum hendrerit nulla. Quisque vitae ultrices massa. Maecenas sollicitudin ligula et velit varius, in sollicitudin libero iaculis. Nulla facilisi. Phasellus dolor turpis, dapibus sed nisi eu, hendrerit laoreet turpis. Duis accumsan pellentesque libero, in auctor sapien convallis non.
         .startTimeline
-      // * ERA
       ul.sectionList
+        // * ERA 1
         li.sectionList_item
+          // * The entire section
           section.lo_sectionEra(style="width: 1000px")
             //.lo_topBar_wrapper
-            ul.lo_topBar_timeline
-              li.timeSlot(style="width: 400px")
+            // * Topbar with line and dots
+            ul.lo_topBar_timeline(style="--topbar-width: 1000px")
+              li.timeSlot(style="width: 200px")
                 .timeSlot_year 1600
                 .timeSlot_dot
                 .timeSlot_line
@@ -39,22 +41,23 @@
                 .timeSlot_year 1700
                 .timeSlot_dot
                 .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1700
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1750
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1800
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1850
-                .timeSlot_dot
-                .timeSlot_line
+              //- li.timeSlot(style="width: 400px")
+              //-   .timeSlot_year 1700
+              //-   .timeSlot_dot
+              //-   .timeSlot_line
+              //- li.timeSlot(style="width: 400px")
+              //-   .timeSlot_year 1750
+              //-   .timeSlot_dot
+              //-   .timeSlot_line
+              //- li.timeSlot(style="width: 400px")
+              //-   .timeSlot_year 1800
+              //-   .timeSlot_dot
+              //-   .timeSlot_line
+              //- li.timeSlot(style="width: 400px")
+              //-   .timeSlot_year 1850
+              //-   .timeSlot_dot
+              //-   .timeSlot_line
+            // * The circle and link in the middle of the era
             .lo_circleEra
               .circleEra
                 img(src="@/assets/media/img/circle1.png")
@@ -62,6 +65,7 @@
                   .circleEra_date 1850-1890
                   h2.circleEra_title Realisme og naturalisme
                   button.btnEra(@click="$router.push('/nb/periods/barokk')") Mer info                  
+            // * List of books
             ul.bookList
               li
                 ButtonBook(
@@ -85,6 +89,104 @@
                   style="left: 800px; top: 150px"
                 )
             SeparatorAuthor
+            // * List of authors
+            ul.authorsList
+              li
+                ButtonAuthor(
+                  name="Peter Christen Asbjørnsen 1",
+                  style="width: 450px; left: 20px; top: 0"
+                )
+              li
+                ButtonAuthor(
+                  name="Charles-Louis Montesquieu 2",
+                  path="/nn/test",
+                  style="width: 239px; left: 50px; top: 50px"
+                )
+              li
+                ButtonAuthor(
+                  name="Peter Christen Asbjørnsen 3",
+                  path="/nn/test",
+                  style="width: 360px; left: 80px; top: 100px"
+                )
+              li
+                ButtonAuthor(
+                  name="Peter Christen Asbjørnsen 4",
+                  path="/nn/test",
+                  style="width: 520px; left: 150px; top: 150px"
+                )
+              li
+                ButtonAuthor(
+                  name="Peter Christen Asbjørnsen 5",
+                  style="width: 520px; left: 800px; top: 150px"
+                )
+        // * ERA 2
+        li.sectionList_item
+          // * The entire section
+          section.lo_sectionEra(style="width: 1400px")
+            //.lo_topBar_wrapper
+            // * Topbar with line and dots
+            ul.lo_topBar_timeline(style="--topbar-width: 1400px")
+              li.timeSlot(style="width: 200px")
+                .timeSlot_year 1750
+                .timeSlot_dot
+                .timeSlot_line
+              li.timeSlot(style="width: 400px")
+                .timeSlot_year 1800
+                .timeSlot_dot
+                .timeSlot_line
+              li.timeSlot(style="width: 400px")
+                .timeSlot_year 1850
+                .timeSlot_dot
+                .timeSlot_line
+              li.timeSlot(style="width: 400px")
+                .timeSlot_year 1900
+                .timeSlot_dot
+                .timeSlot_line
+              //- li.timeSlot(style="width: 400px")
+              //-   .timeSlot_year 1750
+              //-   .timeSlot_dot
+              //-   .timeSlot_line
+              //- li.timeSlot(style="width: 400px")
+              //-   .timeSlot_year 1800
+              //-   .timeSlot_dot
+              //-   .timeSlot_line
+              //- li.timeSlot(style="width: 400px")
+              //-   .timeSlot_year 1850
+              //-   .timeSlot_dot
+              //-   .timeSlot_line
+            // * The circle and link in the middle of the era
+            .lo_circleEra
+              .circleEra
+                img(src="@/assets/media/img/circle1.png")
+                .circleEra_content
+                  .circleEra_date 1850-1890
+                  h2.circleEra_title Realisme og naturalisme
+                  button.btnEra(@click="$router.push('/nb/periods/barokk')") Mer info                  
+            // * List of books
+            ul.bookList
+              li
+                ButtonBook(
+                  title="Om norsk Sprogreformation",
+                  author="Wergeland",
+                  path="/nn/books/wergeland",
+                  style="left: 50px; top: 100px"
+                )
+              li
+                ButtonBook(
+                  title="Kvitebjørnen",
+                  author="Rasmus Løland",
+                  path="/nb/books/loland",
+                  style="left: 100px; top: 200px"
+                )
+              li
+                ButtonBook(
+                  title="Døljen",
+                  author="Vinje",
+                  path="/nn/books/vinje",
+                  style="left: 800px; top: 150px"
+                )
+            SeparatorAuthor
+            // * List of authors
             ul.authorsList
               li
                 ButtonAuthor(
