df63d1f4815c869c462e9af25bfd78f2503405d7
Roger Hestad <roger@doublethink.no>
2021-04-07 23:03:42 +0200

    Timeline: Code cleanup removes old commented out code

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -22,14 +22,6 @@
           p Duis augue tortor, gravida non nisi ut, bibendum hendrerit nulla. Quisque vitae ultrices massa. Maecenas sollicitudin ligula et velit varius, in sollicitudin libero iaculis. Nulla facilisi. Phasellus dolor turpis, dapibus sed nisi eu, hendrerit laoreet turpis. Duis accumsan pellentesque libero, in auctor sapien convallis non.
         .startTimeline
       ul.sectionList
-        // * ERA 0 (component test)
-        //TimelineSection(
-          title="Norrøn tid"
-          from="800"
-          to="1350"
-          id="norron"
-          :yearMarkings="periodYearMarkings[0].yearMarkings"
-          :period="periodRoutes[0]")
         TimelineSection(
           v-for="period in periods" :key="period.id"
           :title="getPeriodTitle(period)"
@@ -38,216 +30,7 @@
           :id="period.id"
           :yearMarkings="period.yearMarkings"
           )
-        // * ERA 1
-        //li.sectionList_item
-          // * The entire section
-          section.lo_sectionEra(style="width: 1000px")
-            //.lo_topBar_wrapper
-            // * Topbar with line and dots
-            ul.lo_topBar_timeline(style="--topbar-width: 1000px")
-              li.timeSlot(style="width: 200px")
-                .timeSlot_year 1600
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1650
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1700
-                .timeSlot_dot
-                .timeSlot_line
-              //- li.timeSlot(style="width: 400px")
-              //-   .timeSlot_year 1700
-              //-   .timeSlot_dot
-              //-   .timeSlot_line
-              //- li.timeSlot(style="width: 400px")
-              //-   .timeSlot_year 1750
-              //-   .timeSlot_dot
-              //-   .timeSlot_line
-              //- li.timeSlot(style="width: 400px")
-              //-   .timeSlot_year 1800
-              //-   .timeSlot_dot
-              //-   .timeSlot_line
-              //- li.timeSlot(style="width: 400px")
-              //-   .timeSlot_year 1850
-              //-   .timeSlot_dot
-              //-   .timeSlot_line
-            // * The circle and link in the middle of the era
-            .lo_circleEra
-              .circleEra
-                img(src="@/assets/media/img/circle1.png")
-                .circleEra_content
-                  .circleEra_date 1850-1890
-                  h2.circleEra_title Realisme og naturalisme
-                  button.btnEra(@click="$router.push('/nb/periods/barokk')") Mer info                  
-            // * List of books
-            ul.bookList
-              li
-                ButtonBook(
-                  title="Om norsk Sprogreformation",
-                  author="Wergeland",
-                  path="/nn/books/wergeland",
-                  style="left: 50px; top: 100px"
-                )
-              li
-                ButtonBook(
-                  title="Kvitebjørnen",
-                  author="Rasmus Løland",
-                  path="/nb/books/loland",
-                  style="left: 100px; top: 200px"
-                )
-              li
-                ButtonBook(
-                  title="Døljen",
-                  author="Vinje",
-                  path="/nn/books/vinje",
-                  style="left: 800px; top: 150px"
-                )
-            SeparatorAuthor
-            // * List of authors
-            ul.authorsList
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 1",
-                  style="width: 450px; left: 20px; top: 0"
-                )
-              li
-                ButtonAuthor(
-                  name="Charles-Louis Montesquieu 2",
-                  path="/nn/test",
-                  style="width: 239px; left: 50px; top: 50px"
-                )
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 3",
-                  path="/nn/test",
-                  style="width: 360px; left: 80px; top: 100px"
-                )
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 4",
-                  path="/nn/test",
-                  style="width: 520px; left: 150px; top: 150px"
-                )
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 5",
-                  style="width: 520px; left: 800px; top: 150px"
-                )
-        // * ERA 2
-        //li.sectionList_item
-          // * The entire section
-          section.lo_sectionEra(style="width: 1400px")
-            //.lo_topBar_wrapper
-            // * Topbar with line and dots
-            ul.lo_topBar_timeline(style="--topbar-width: 1400px")
-              li.timeSlot(style="width: 200px")
-                .timeSlot_year 1750
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1800
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1850
-                .timeSlot_dot
-                .timeSlot_line
-              li.timeSlot(style="width: 400px")
-                .timeSlot_year 1900
-                .timeSlot_dot
-                .timeSlot_line
-              //- li.timeSlot(style="width: 400px")
-              //-   .timeSlot_year 1750
-              //-   .timeSlot_dot
-              //-   .timeSlot_line
-              //- li.timeSlot(style="width: 400px")
-              //-   .timeSlot_year 1800
-              //-   .timeSlot_dot
-              //-   .timeSlot_line
-              //- li.timeSlot(style="width: 400px")
-              //-   .timeSlot_year 1850
-              //-   .timeSlot_dot
-              //-   .timeSlot_line
-            // * The circle and link in the middle of the era
-            .lo_circleEra
-              .circleEra
-                img(src="@/assets/media/img/circle1.png")
-                .circleEra_content
-                  .circleEra_date 1850-1890
-                  h2.circleEra_title Realisme og naturalisme
-                  button.btnEra(@click="$router.push('/nb/periods/barokk')") Mer info                  
-            // * List of books
-            ul.bookList
-              li
-                ButtonBook(
-                  title="Om norsk Sprogreformation",
-                  author="Wergeland",
-                  path="/nn/books/wergeland",
-                  style="left: 50px; top: 100px"
-                )
-              li
-                ButtonBook(
-                  title="Kvitebjørnen",
-                  author="Rasmus Løland",
-                  path="/nb/books/loland",
-                  style="left: 100px; top: 200px"
-                )
-              li
-                ButtonBook(
-                  title="Døljen",
-                  author="Vinje",
-                  path="/nn/books/vinje",
-                  style="left: 800px; top: 150px"
-                )
-            SeparatorAuthor
-            // * List of authors
-            ul.authorsList
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 1",
-                  style="width: 450px; left: 20px; top: 0"
-                )
-              li
-                ButtonAuthor(
-                  name="Charles-Louis Montesquieu 2",
-                  path="/nn/test",
-                  style="width: 239px; left: 50px; top: 50px"
-                )
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 3",
-                  path="/nn/test",
-                  style="width: 360px; left: 80px; top: 100px"
-                )
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 4",
-                  path="/nn/test",
-                  style="width: 520px; left: 150px; top: 150px"
-                )
-              li
-                ButtonAuthor(
-                  name="Peter Christen Asbjørnsen 5",
-                  style="width: 520px; left: 800px; top: 150px"
-                )
 
-//#timeline 
-  h1 Timeline - {{ globalVars.langCode }}
-  div(v-if="periodRoutes")
-    h2 Perioder
-    ul
-      li(v-for="period in periodRoutes", :key="period.name") {{ period.meta.from }} - {{ period.meta.to }} <router-link :to="period.path">{{ period.meta.title }}</router-link>
-
-    h2 Bøker
-    ul
-      li(v-for="book in bookRoutes", :key="book.meta.title") {{ book.meta.year }} - <router-link :to="book.path">{{ book.meta.title }}</router-link> av {{ book.meta.author }}
-
-    h2 Forfattere
-    ul
-      li(v-for="artist in artists", :key="artist.NAVN") {{ artist.FRA }} - {{ artist.TIL }} {{ artist.NAVN }}
-        div(v-html="artist.OMTALE")
 </template>
 
 <script>
@@ -284,24 +67,6 @@ export default {
       bookRoutes: null,
       periodRoutes: null,
       isDraggable: false, // * Debug var making it easy to turn off dragging
-      periodYearMarkings: [
-        {
-          periodId: "norron",
-          yearMarkings: [
-            [800, 42],
-            [850, 44],
-            [900, 47],
-            [950, 52],
-            [1000, 55],
-            [1050, 60],
-            [1100, 65],
-            [1150, 71],
-            [1200, 78],
-            [1250, 86],
-            [1300, 95],
-          ],
-        },
-      ],
       periods: periods,
     };
   },
@@ -325,15 +90,6 @@ export default {
       if (this.globalVars.langCode == "nn") return period["nnTitle"];
 
       return period["nbTitle"];
-    },
-    getPeriodYearMarkings(periodPath) {
-      const periodId = this.getPeriodId(periodPath);
-      const yearMarkingObject = this.periodYearMarkings.find(
-        period => period.periodId == periodId,
-      );
-      if (yearMarkingObject) return yearMarkingObject.yearMarkings;
-
-      return [[1000, 1000]];
     },
     onTimelineDrag(e) {},
   },
