b7b31f59d2e4aff1c9cbacdc1cef72ff79531ed8
Roger Hestad <roger@doublethink.no>
2021-04-07 22:58:02 +0200

    tools/GenerateBookRoutes: Adds code to make booksData.js file
    Sine a lot of metadata is the same for the two languages it made sence to put them in a separate file

--- tools/GenerateBookRoutes.js
+++ tools/GenerateBookRoutes.js
@@ -31,6 +31,8 @@ async function generateFile() {
     console.log("orgJson[0] = ", orgJson[0]);
 
     let routeString = "[\n";
+
+    let booksJsString = "export const books = [\n";
     for (let i = 0; i < orgJson.length; i++) {
       let tmpName = orgJson[i].ID;
       if (args.lang == "nb") tmpName = tmpName.slice(0, -2);
@@ -52,6 +54,7 @@ async function generateFile() {
         },
       },
       `;
+
       // * Bokmål book
       routeString += `{
         path: '/${lcNB}/books/${tmpName.toLowerCase()}',
@@ -66,6 +69,19 @@ async function generateFile() {
           default: () =>
           import(/* webpackChunkName: '${tmpName.toLowerCase()}${lcNB}' */ '../books/${tmpName.toLowerCase()}/${tmpName}${ucNB}.vue'),
         },
+      },
+      `;
+
+      // Only one version since the data is the same in the NN and NB original
+      booksJsString += `{
+        id: '${tmpName.toLowerCase()}',
+        nnTitle: '${orgJson[i].TITTEL}',
+        nbTitle: '${orgJson[i].TITTEL}',
+        author: '${tmpName}',
+        year: ${orgJson[i]["ÅR"]},
+        top: 0,
+        left: 0,
+        period: '',
       },
       `;
 
@@ -85,11 +101,16 @@ async function generateFile() {
     }
 
     routeString += "];\n";
+    booksJsString += "]\n";
     fileString += routeString;
 
     const outputFilename = "books.js";
     // console.log("routeString = ", routeString);
     await fs.outputFile(outputFilename, fileString);
+
+    const bookJsFileName = "booksData.js";
+    fileString = booksJsString;
+    await fs.outputFile(bookJsFileName, fileString);
   } catch (err) {
     console.error(err);
   }
