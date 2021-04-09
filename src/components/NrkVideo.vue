8280ef6046bd040c71a5f1702aa2cf5be4356b42
Roger Hestad <roger@doublethink.no>
2021-04-09 11:09:30 +0200

    Adds NrkVideo component
    It uses the video ID as a prop

---
+++ src/components/NrkVideo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="videoWrapper">
+    <div class="nrk-video" :data-nrk-id="id">&nbsp;</div>
+    <!-- <script src="https://static.nrk.no/ludo/latest/video-embed.js"></script> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NrkVideo",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    console.log("NrkVideo.mounted: videoWrapper = ", this.$refs.videoWrapper);
+    // let scriptElement = this.$refs.videoWrapper.createElement("script");
+    let scriptElement = document.createElement("script");
+    scriptElement.setAttribute(
+      "src",
+      "https://static.nrk.no/ludo/latest/video-embed.js",
+    );
+    this.$refs.videoWrapper.appendChild(scriptElement);
+  },
+};
+</script>
