ddb81d80538145fade3c1ed8c7614e694630ddff
Roger Hestad <roger@doublethink.no>
2021-05-10 09:57:32 +0200

    Barokk period text in both languages

--- src/periods/barokk/BarokkNB.vue
+++ src/periods/barokk/BarokkNB.vue
@@ -1,55 +1,110 @@
 <template lang="pug">
 PagePeriod
   // Content here
-  h3 Heimstaddiktning
-  p Eksempel på link til boken <router-link to="/nn/books/asbjornsenmoe">Norske folkeeventyr</router-link>
+  h3 Tida og samfunnsforholdene 
+  //p Eksempel på link til boken <router-link to="/nn/books/asbjornsenmoe">Norske folkeeventyr</router-link>
 
+  h5 Kirken mister makt
   p
-    | Tida rundt århundreskiftet var preget av motstridende tankestrømmer. På den ene siden skapte den teknologiske utviklingen en forventning om nye og bedre tider. Mange flyttet fra landet til byene for å få seg arbeid i ulike bedrifter og fabrikker. På den andre siden savnet folk levemåten i bondesamfunnet som de kom fra. De fryktet at samfunnet slik de kjente det, ville gå tapt, og de lengtet hjem.
-  p
-    | Litteraturen i 1890-årene gjenspeilte denne uroen, og det utviklet seg en skrivetradisjon som seinere fikk betegnelsen heimstaddiktning. I denne tradisjonen vokste det fram en vilje til å ta vare på og dyrke det folkelige.
-  p
-    | Med heimstaddiktning mener vi at forfatterne skrev om miljøet på hjemstedet sitt. De ville framheve egenarten ved landsdelen eller hjemstedet, skildre det særegne ved miljø, folk og natur.
-  p
-    | Storbylivet ble i denne typen diktning framstilt som noe usunt og som en motsetning til livet på landsbygda eller i småbyen. En vanlig oppfatning var at det urbane livet førte til rotløshet, mens livet på landet gav ro i sjela. Det var her folk hadde røttene sine.
-  blockquote
-    | «Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced.»
-    br
-    | (My Author)
-  h3 Rasmus Løland – en pioner i norsk barnelitteratur
-  p
-    | I 1892 gav Nordahl Rolfsen ut Læsebog for folkeskolen. Boka fikk mye å si for framveksten av en egen barnelitteratur i Norge. Skolefolk mente at det var viktig med tilgang til kvalitetslitteratur for å fremme leseevnen hos de yngste. Samtidig ble det åpnet skoleboksamlinger (bibliotek) rundt om i landet.
-  p
-    | Rasmus Løland (1861–1907) var en av pionerene i moderne norsk barnelitteratur. Han kom fra Ryfylke i Rogaland og skrev i alt 30 bøker, de aller fleste for barn. Han var banebrytende som barnebokforfatter fordi diktningen hans var blottet for moralisme og sentimentalitet. Barneverdenen til Løland var realistisk og fylt av barnlig fantasi og fabulering.
-  p
-    | Perioden fra 1890 til 1910 var en gullalder for norsk barnelitteratur. Hos landsmålsforfattere som Rasmus Løland, Per Sivle og Hans Seland finner vi en sterk forankring i det hjemlige og lokale, og noe av det samme ser vi hos Gabriel Scott og Hans Aanrud.
-  p
-    | Utover på 1900-tallet videreførte Marie Hamsun og Halvor Floden barneboka og heimstadtradisjonen. I 1930-årene var det nyrealistiske strømninger som satte farge på barnelitteraturen, for eksempel i bøkene til Ingeborg Refling Hagen og Kåre Holt.
-  a(href='https://www.google.no') Rasmus Løland biography on Wikipedia        
-  h3 Kvitebjørnen
-  p
-    | Barneboka Kvitebjørnen var høydepunktet i Lølands produksjon. Boka utkom i 1906, året før han døde. Handlingen foregår i et bygdemiljø, og i sentrum står fire gutter i alderen ni til tolv år. Vi følger dem gjennom ni kapitler, fra den første snøen faller seinhøstes og fram til våren kommer på nytt.
-  p
-    | Løland lar barna dagdrømme og fantasere. Blant annet lager de en stor bjørn av snø. De graver ut snøbjørnen og drar av gårde på en fantasireise til Nordpolen. Guttene er også på alle andre måter som barn ellers: De rømmer hjemmefra, de er i opposisjon til de voksne, og de gjør ikke alltid slik foreldrene har formanet dem om. For eksempel blir en lekmannspredikant som kommer til bygda, utsatt for snøballkasting.
-  p
-    | I den samme boka gir Løland også et innblikk i lekmannsbevegelsen på Vestlandet seint på 1800-tallet. De lekmannskristne mente det ikke var tilstrekkelig å tilhøre kirka, en skulle i tillegg ha gjennomgått en åndelig vekkelse for å kunne kalle seg ekte kristen. I Kvitebjørnen blir lekmannsbevegelsen skyteskive for Løland både i bokstavelig og overført betydning.
-  a(href='https://www.google.no') Kvitebjørnen on wikipedia
-  h3 En antididaktisk skrivemåte
-  p
-    | Kvitebjørnen bryter med den tradisjonelle didaktiske barneboka, som var moraliserende, advarende og formanende. Det blir sagt om Løland at han bruker en antididaktisk skrivemåte, særlig i Kvitebjørnen, og at han var en forløper for den moderne, realistiske barneboklitteraturen som vokste fram på 1900-tallet.
-  p
-    | Rasmus Løland bryter med lange, nedarvete opplæringstradisjoner og tradisjonelle rollemønstre. Han har evnen til å sette seg inn i hvordan barn tenker og føler, og han har innsikt i barns sjeleliv.
-  p
-    | I 1959 kom Kvitebjørnen ut i ny utgave, forkortet og dels omskrevet av Halldis Moren Vesaas.
-  h3 En pådriver for landsmålet
-  img(src='https://dummyimage.com/1280x720/cecece/ffffff&text=myName' alt='')
-  p
-    | Rasmus Løland engasjerte seg sterkt i målsaken. Han var en ivrig pådriver for nynorsk teater, og han var med på å stifte Noregs Ungdomslag. I tillegg var han en sentral person i unionsspørsmålet og en forkjemper for demokratisering og stemmerett.
-  p
-    | I første del av forfatterskapet var Løland ivrig purist. Han mente at en ikke skulle bruke ord som kom fra tysk eller dansk. Seinere skiftet han holdning i dette spørsmålet, han syntes det viktigste var at målet var norsk i stil og tonelag.
-  p
-    | Arbeidet for norsk mål var nok en viktig grunn til at Løland skrev for barn. Han ville skape interesse for litteratur og lesing hos de yngste, og denne litteraturen skulle være på et mål som barna følte seg trygge på. Tanken var at en på denne måten skulle vinne kampen om skriftmålet etter hvert.
+    | Kirken mister mye av sin makt etter middelalderen, men denne makta går ikke til folket. Det er kongene i de ulike landene i Europa som samler makta og får større kontroll over kirken. 
 
+  h5 Konge av Guds nåde
+  p
+    | De som har makt i samfunnet, blir dyrket. Det er stor avstand mellom de få rike og resten av folket. Herskerne utnytter religionen til å rettferdiggjøre makt og posisjon. Ifølge Luther har kongen fått sin makt fra Gud fordi all makt kommer fra Gud.   
+
+  h5 Eneveldet  
+  p
+    | I 1660 blir Fredrik 3. eneveldig konge over Danmark og Norge. Det vil si at han får ubegrenset makt. Kongen konfiskerer deler av de kirkelige eiendommene etter reformasjonen og skaffer seg dermed mer rikdom.  
+
+  h5 Merkantilisme  
+  p
+    | Begrepet merkantilisme stammer fra opplysningstida. De store sjøfartsnasjonene driver utstrakt handelsvirksomhet, og ideen er å eksportere flere varer enn de importerer. På den måten legger en til rette for større statsmakt, der kongen styrer all handel og skjermer produksjonen. Dette fører i mange tilfeller til konflikt mellom ulike stater, og mellom kongen og det nye handelsborgerskapet.
+
+  h3 Litt om tenkemåten 
+
+  h5 Eneveldet  
+  p
+    | Samfunnsbildet (verdensbildet) kan sammenliknes med en pyramide der alle mennesker har sin faste plass. På toppen står kongen, og over kongen står bare Gud. Under kommer de adelige, deretter borgerskapet med handelsmenn og håndverkere. Bønder, leilendinger og husmenn står nederst i hierarkiet.  
+
+  h5 Grip dagen, men husk at du skal dø  
+  p
+    | Menneskenes livsholdning på 1600-tallet kan karakteriseres med de to latinske uttrykkene Carpe diem, som betyr ”Grip dagen”, og Memento mori, som betyr ”Husk at du skal dø”. Livet er kort, alt er forgjengelig, så grip dagen og lev mens du kan. Døden venter, og den kommer vi ikke utenom. Disse to uttrykkene sier noe om den motsetningsfylte livsholdningen som ofte trer fram i barokk kunst.  
+
+  h5 Idealisme  
+  p
+    | En del tenkere på 1600-tallet mener at tilværelsen er av sjelelig eller åndelig karakter. En slik holdning kalles idealisme. I motsetning til materialismen ser idealismen verden som noe som utgår fra bevisstheten. En idealist i moderne språkbruk er en som handler ut fra visse idealer. 
+
+  h5 Materialisme 
+  p
+    | Andre mener at alt kan tilbakeføres til fysiske størrelser. En slik holdning kaller vi materialisme. Den engelske legen William Harvey oppdaget i 1628 at hjertet bare var en muskel som pumpet blodet rundt i menneskekroppen. En kollega av Harvey uttrykte: Video sed non credo – ”Jeg ser det, men jeg tror det ikke”. Denne konflikten mellom gamle, fastgrodde forestillinger og vitenskapelige bevis preger barokken. Materialismen avviser med andre ord at det eksisterer noe som er overnaturlig, og hevder at alt lar seg forklare vitenskapelig. 
+
+  h3 Kjennetegn ved barokkstilen 
+
+  h5 Barokken som stilart 
+  p
+    | Barokken er betegnelsen på en stilart som oppstår i Roma på slutten av 1500-tallet. I tillegg er barokken navnet på en kulturhistorisk epoke som fyller hele 1600-tallet. Barokken omfatter europeisk kunst, arkitektur, musikk og litteratur. I Norge ser vi påvirkningen noe seinere enn i resten av Europa, og her i landet regner vi derfor den barokke perioden fra ca. 1650 til 1750.  
+  h5 Den katolske kirken som pådriver  
+  p
+    | Kirken bruker den barokke stilarten til å manifestere sin makt. Vi kan derfor se barokken som en motreaksjon til renessansen. Det aller meste av barokk kunst er kirkekunst, og det meste av litteraturen er salmer og diktning knyttet til kirken. 
+  h5 En prangende stil 
+  p
+    | I barokken møter vi en pompøs og prangende livsutfoldelse, med vekt på det overdådige. Skulptur- og malerkunst, barokk arkitektur og møbelstil er preget av en slik overveldende utsmykking. Det spares ikke på verken materialer eller effekter.  
+  h5 Konflikt og dramatikk  
+  p
+    | Heller ikke forfatterne er beskjedne i bruk av virkemidler. De tar i bruk svulstige uttrykk og sterke metaforer. Overdrivelser, motsetninger og et sterkt og dramatisk bildespråk er kjennetegn på barokk kunst av alle slag. Operaen som scenekunst vokste fram i den barokke perioden, og store, påkostede operaforestillinger kom på mote.  
+  h5 Det åndelige og det verdslige  
+  p
+    | Gud står i sentrum i barokken, men kunsten er likevel dualistisk. Det vil si at kunstneren fokuserer både på mennesket (det verdslige) og på Gud (det åndelige). Kunstneren fokuserer altså både på dette livet og på livet etter døden.  
+  h5 Sterke kontraster  
+  p
+    | Barokken preges av sterke kontraster. Disse kontrastene kommer også til uttrykk i musikken. Barokkmusikk er lett å dra kjensel på med sine sterke kontraster, slik vi kjenner det gjennom De fire årstidene av den italienske komponisten Vivaldi. Andre kjente komponister fra perioden er Bach og Händel (Tyskland). Begge er særlig kjent for å ha komponert kirkemusikk.
+
+  h3 Litteraturen 
+
+  h5 Religiøs litteratur 
+  p
+    | Den barokke litteraturen er først og fremst religiøs, og det er salmediktningen som dominerer. Gjennomgangsmotivet i de barokke salmene er dødstanken: alt skal forgå – det virkelige livet er i himmelen. Slik sett er det ofte en lengsel etter døden som kommer til uttrykk. 
+
+  h5 Europeiske forfattere 
+  p
+    | I Italia er Giambattista Marino den fremste barokkdikteren. I Spania er det Francisco de Quevedo, og i England er det John Donne og John Milton. I Tyskland skriver Martin Luther barokke salmer. Han inspirerer nok Thomas Kingo, som skulle bli den største nordiske salmedikteren i denne perioden.  
+
+  h5 Thomas Kingo (1634–1703) 
+  p
+    | er dansk dikter og biskop. I 1699 kommer det ut ei salmebok som inneholder 86 av salmene hans. Denne boka – Kingos salmebok – blir godkjent av kongen til bruk i alle kirker i Danmark og Norge, og er i bruk helt fram til 1870-årene. Boka finner du på Tidslinja.  
+
+  h5 Dorothe Engelbretsdotter (1634–1716)  
+  p
+    | blir født og lever i Bergen. Hun er populær i samtida si og kan leve av boksalget. Det er ganske utrolig at en kvinne kan klare det på 1600-tallet. Men i privatlivet opplever hun mye motgang og sorg. Boka Siælens Sang-Offer finner du på Tidslinja.  
+
+  h5 Petter Dass (1647–1707)  
+  p
+    | kommer fra Nordland og er en populær mann i samtida. Han regnes for vår største barokkdikter, selv om stilen hans er mindre tidstypisk enn stilen til Kingo og Engelbretsdotter. Boka Nordlands Trompet finner du på Tidslinja.
+
+  h3 Litt om språket  
+  h5 Talemålet: fra norrønt til moderne norsk 
+  p
+    | Det norrøne talemålet er borte, og det moderne norske talespråket (dialektene) har festet seg, ikke ulikt den formen de har i dag.  
+  h5 Skillet mellom vestnorsk og østnorsk  
+  p
+    | Det norrøne skriftspråket er utdødd, men talespråket har overlevd og utviklet seg videre. Skillet mellom øst og vest etablerer seg på 1500-tallet. 
+  h5 Bergen og nedertysk  
+  p
+    | Bergen og Vestlandet er blitt sterkt språklig påvirket av den hanseatiske (tysk-nederlandske) handelsvirksomheten som har foregått helt siden 1200-tallet. Denne påvirkningen fortsetter og farger både talemålet og skriftspråket i Bergen.  
+  h5 Bibelen på dansk  
+  p
+    | Det kirkelige språket er dansk fordi det ble brukt av danske prester. Både bibel, katekisme og salmebok er trykt på dansk. Bibelen kommer ut på dansk i 1550, og det er denne oversettelsen som blir brukt her i landet helt fram til 1904. 
+  h5 Administrasjonsspråket 
+  p
+    | Dansk er administrasjonsspråket på 1600-tallet. De fleste embetsmenn er danske, og de skriver og snakker dansk. Offentlige dokumenter og sakspapir (såkalte diplomer) blir skrevet på dansk.  
+  h5 Bygdemålstekster  
+  p
+    | Det finnes likevel tekster med innslag av dialekt og norsk syntaks. Dette er tekster som blir brukt i bryllup og festlige lag. Det finnes også jordbøker (dokumenter om eiendommer), lover og brev der teksten er farget av forfatterens dialekt. Slike tekster kaller vi gjerne bygdemålstekster.  
+  h5 Norvagismer  
+  p
+    | Både Petter Dass og Dorothe Engelbretsdotter skriver på dansk, men de bruker norske ord og uttrykk enkelte steder. Slike ord kaller vi norvagismer. Et eksempel kan være foss for det danske ordet ”vandfald”. Petter Dass brukte ellers norske ord når han omtalte fiskeslag og fangstredskaper.
+
+  h3 Bildekunst og arkitektur 
 </template>
 
 <script>
--- src/periods/barokk/BarokkNN.vue
+++ src/periods/barokk/BarokkNN.vue
@@ -1,7 +1,122 @@
 <template lang="pug">
 PagePeriod
   // Content here
+  h3 Tida og samfunnstilhøva 
 
+  h5 Kyrkja mister makt 
+  p
+    | Kyrkja mister mykje av den makta ho hadde i mellomalderen, men denne makta går ikkje til folket. Det er kongane i dei ulike landa i Europa som samlar makta og får større kontroll over kyrkja. 
+
+  h5 Konge av Guds nåde 
+  p
+    | Dei som har makt i samfunnet, blir dyrka. Det er stor avstand mellom dei få rike og resten av folket. Herskarane utnyttar religionen til å rettferdiggjere makt og posisjon. Ifølgje Luther har kongen fått makta si frå Gud fordi all makt kjem frå Gud. 
+
+  h5 Eineveldet 
+  p
+    | I 1660 blir Fredrik 3. eineveldig konge over Danmark og Noreg. Det vil seie at han får uavgrensa makt. Kongen konfiskerer delar av dei kyrkjelege eigedommane etter reformasjonen og skaffar seg dermed meir rikdom. 
+
+  h5 Merkantilisme 
+  p
+    | Omgrepet merkantilisme stammar frå opplysningstida. Dei store sjøfartsnasjonane driv stor handelsverksemd, og ideen er å eksportere fleire varer enn dei importerer. På den måten legg ein til rette for større statsmakt, der kongen styrer all handel og skjermar produksjonen. Dette fører i mange tilfelle til konflikt mellom ulike statar, og mellom kongen og det nye handelsborgarskapet  
+
+  h3 Litt om tenkjemåten 
+
+  h5 Eineveldet 
+  p 
+    | Samfunnsbiletet (verdsbiletet) kan samanliknast med ein pyramide der alle menneska har sin faste plass. På toppen står kongen, og over kongen står berre Gud. Under kjem dei adelege, deretter borgarskapet med handelsmenn og handverkarar. Bønder, leiglendingar og husmenn står nedst i hierarkiet. 
+
+  h5 Grip dagen, men hugs at du skal døy 
+  p 
+    | Livshaldninga til menneska på 1600-talet kan karakteriserast med dei to latinske uttrykka Carpe diem, som tyder ”Grip dagen”, og Memento mori, som tyder ”Hugs at du skal døy”. Livet er kort, alt er forgjengeleg, så grip dagen og lev mens du kan. Døden ventar, og den kjem vi ikkje utanom. Desse to uttrykka seier noko om den motsetningsfylte livshaldninga som ofte syner seg i barokk kunst. 
+
+  h5 Idealisme 
+  p 
+    | Ein del tenkjarar på 1600-talet meiner at tilværet er av sjeleleg eller åndeleg karakter. Ei slik haldning kallar vi idealisme. I motsetning til materialismen ser idealismen verda som noko som går ut frå medvitet. Ein idealist i moderne språkbruk er ein som handlar ut frå visse ideal. 
+
+  h5 Materialisme 
+  p 
+    | Andre meiner at alt kan førast attende til fysiske storleikar. Ei slik haldning kallar vi materialisme. Den engelske legen William Harvey oppdaga i 1628 at hjartet berre var ein muskel som pumpa blodet rundt i menneskekroppen. Ein kollega av Harvey uttrykte: Video sed non credo – ”Eg ser det, men eg trur det ikkje”. Denne konflikten mellom gamle, fastgrodde førestillingar og vitskaplege prov pregar barokken. Materialismen avviser med andre ord at det eksisterer noko som er overnaturleg, og hevdar at alt lét seg forklare vitskapleg. 
+
+  h3 Kjennemerke ved barokkstilen 
+
+  h5 Barokken som stilart 
+  p 
+    | Barokken er nemninga på ein stilart som oppstår i Roma på slutten av 1500-talet. I tillegg er barokken namnet på ein kulturhistorisk epoke som fyller heile 1600-talet. Barokken omfattar europeisk kunst, arkitektur, musikk og litteratur. I Noreg ser vi påverknaden noko seinare enn i resten av Europa, og her i landet reknar vi derfor den barokke perioden frå ca. 1650 til 1750. 
+
+  h5 Den katolske kyrkja som pådrivar 
+  p 
+    | Kyrkja bruker den barokke stilarten til å manifestere makta si. Vi kan derfor sjå barokken som ein motreaksjon til renessansen. Det aller meste av barokk kunst er kyrkjekunst, og det meste av litteraturen er salmar og dikting knytt til kyrkja. 
+
+  h5 Ein prangande stil 
+  p 
+    | I barokken møter vi ei pompøs og prangande livsutfolding, med vekt på det overdådige. Skulptur- og målarkunst, barokk arkitektur og møbelstil er prega av ei slik overveldande utsmykking. Det blir ikkje spart på verken materiale eller effektar.  
+
+  h5 Konflikt og dramatikk 
+  p 
+    | Heller ikkje forfattarane er smålåtne i bruken av verkemiddel. Dei bruker svulstige uttrykk og sterke metaforar. Overdriving, motsetningar og eit sterkt og dramatisk biletspråk er kjennemerke på barokk kunst av alle slag. Operaen som scenekunst voks fram i den barokke perioden, og store, påkosta operaframsyningar kom på mote.  
+
+  h5 Det åndelege og det verdslege  
+  p 
+    | Gud står i sentrum i barokken, men kunsten er likevel dualistisk. Det vil seie at kunstnaren fokuserer på både mennesket (det verdslege) og Gud (det åndelege). Kunstnaren fokuserer altså både på dette livet og på livet etter døden. 
+
+  h5 Sterke kontrastar 
+  p 
+    | Det er dei sterke kontrastane som pregar barokken. Desse kontrastane kjem òg til uttrykk gjennom musikken. Barokk musikk er lett å dra kjensel på med sine sterke kontrastar, slik vi kjenner det gjennom Dei fire årstidene av den italienske komponisten Vivaldi. Andre kjende komponistar frå perioden er Bach og Händel (Tyskland). Begge er særleg kjende for å komponere kyrkjemusikk.
+
+
+  h3 Litteraturen 
+
+  h5 Religiøs litteratur 
+  p
+    | Den barokke litteraturen er først og fremst religiøs, og det er salmediktinga som dominerer. Gjennomgangsmotivet i dei barokke salmane er dødstanken: alt skal gå under – det verkelege livet er i himmelen. Slik sett er det ofte ein lengt etter døden som kjem til uttrykk. 
+
+  h5 Europeiske forfattarar 
+  p
+    | I Italia er Giambattista Marino den fremste barokkdiktaren. I Spania er det Francisco de Quevedo, og i England er det John Donne og John Milton. I Tyskland skriv Martin Luther barokke salmar. Han inspirerer nok Thomas Kingo, som skulle bli den største nordiske salmediktaren i denne perioden. 
+  
+  h5 Thomas Kingo (1634–1703) 
+  p
+    | er dansk diktar og biskop. I 1699 kjem det ut ei salmebok som inneheld 86 av salmane hans. Denne boka – Kingos salmebok – blir godkjend av kongen til bruk i alle kyrkjer i Danmark og Noreg, og er i bruk heilt fram til 1870-åra. Boka finn du på Tidslinja. 
+
+  h5 Dorothe Engelbretsdotter (1634–1716) 
+  p
+    | er fødd og lever i Bergen. Ho er populær i samtida si og kan leve av boksalet. Det er ganske utruleg at ei kvinne kan klare det på 1600-talet. Men i privatlivet opplever ho mykje motgang og sorg. Boka Siælens Sang-Offer finn du på Tidslinja. 
+
+  h5 Petter Dass (1647–1707) 
+  p
+    | kjem frå Nordland og er ein populær mann i si samtid. Han blir rekna for vår største barokkdiktar, sjølv om stilen hans er mindre tidstypisk enn stilen til Kingo og Engelbretsdotter. Boka Nordlands Trompet finn du på Tidslinja. 
+
+  h3 Litt om språket  
+
+  h5 Talemålet: frå norrønt til moderne norsk 
+  p
+    | Det norrøne talemålet er borte, og det moderne norske talespråket (dialektane) har festa seg, ikkje ulikt den forma dei har i dag. 
+
+  h5 Skiljet mellom vestnorsk og austnorsk 
+  p
+    | Det norrøne skriftspråket er utdøydd, men talespråket har overlevd og utvikla seg vidare. Skiljet mellom aust og vest festar seg på 1500-talet. 
+
+  h5 Bergen og nedertysk 
+  p
+    | Bergen og Vestlandet har hatt ein sterk språkleg påverknad frå den hanseatiske (tysk-nederlandske) handelsverksemda som har gått føre seg heilt sidan 1200-talet. Denne påverknaden held fram og fargar både talemålet og skriftspråket i Bergen. 
+
+  h5 Bibelen på dansk 
+  p
+    | Det kyrkjelege språket er dansk fordi det blei brukt av danske prestar. Både bibel, katekisme og salmebok er prenta på dansk. Bibelen kjem ut på dansk i 1550, og det er denne omsetjinga som blir brukt her i landet heilt fram til 1904. 
+
+  h5 Administrasjonsspråket 
+  p
+    | Dansk er administrasjonsspråket på 1600-talet. Dei fleste embetsmenn er danske, og dei skriv og taler dansk. Offentlege dokument og sakspapir (såkalla diplom) blir skrivne på dansk. 
+
+  h5 Bygdemålstekstar  
+  p
+    | Det finst likevel tekstar med innslag av dialekt og norsk syntaks. Dette er tekstar som blir nytta til bryllaup og festlege lag. Det finst også jordbøker (dokument om eigedommar), lover og brev der teksten er farga av dialekten til forfattaren. Slike tekstar kallar vi gjerne bygdemålstekstar. 
+
+  h5 Norvagismar 
+  p
+    | Både Petter Dass og Dorothe Engelbretsdotter skriv på dansk, men dei bruker norske ord og uttrykk somme stader. Slike ord kallar vi norvagismar. Eit eksempel kan vere foss for det danske ordet ”vandfald”. Petter Dass brukte elles norske ord når han omtalte fiskeslag og fangstreiskap.  
+
+  h3 Biletkunst og arkitektur
 </template>
 
 <script>
