309606d0e6f9c0ac69b905e4c2539aaa742f011c
Roger Hestad <roger@doublethink.no>
2021-05-19 10:53:16 +0200

    Adds ImageTextBlock component
    Displays an image to the left and a slot to the right

---
+++ src/components/ImageTextBlock.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+.flex.flex-wrap
+  .col(:class="'col' + imageColumns")
+    figure.figure-left.mt5
+      img(:src="image" :alt="altText")
+      figcaption(v-if="imageCaption !== ''") {{ imageCaption }}
+  .col(:class="'col' + textWidth")
+    slot
+</template>
+
+<script>
+/**
+ * ImageTextBlock
+ *
+ * Displays an image to the left and slot content to the right.
+ *
+ * The width of the image is configured using imageColumns. The slot content
+ * will take up the rest of the space.
+ *
+ */
+export default {
+  props: {
+    image: {
+      type: String,
+      default: "",
+    },
+    // * The number of columns the image should occupy
+    // * Page width is 12 columns
+    imageColumns: {
+      type: Number,
+      default: 4,
+    },
+    imageCaption: {
+      type: String,
+      default: "",
+    },
+    altText: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    textWidth() {
+      return 12 - this.imageColumns;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
