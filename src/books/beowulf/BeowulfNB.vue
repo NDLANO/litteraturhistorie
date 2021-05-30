6a194f262bc3e63f667e1e5790332e7493dbfc7d
Roger Hestad <roger@doublethink.no>
2021-05-30 18:00:52 +0200

    Beowulf: Adds images

--- src/books/beowulf/BeowulfNB.vue
+++ src/books/beowulf/BeowulfNB.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
-PageBook
+PageBook(:showAuthor="false")
+
+  figure.col.col6.right-image
+    img(src="./Beowulf_utklipp.jpg" alt="Første side av ”Beowulf”-kvadet i et manuskript skrevet en gang mellom 700 og 1000")
+    figcaption Første side av ”Beowulf”-kvadet i et manuskript skrevet en gang mellom 700 og 1000.
+      NdlaAudio(source="media/books/beowulf/beowulf_sitat_britishlibary.mp3")
+      a(href="http://www.heimskringla.no/wiki/Beowulf") Les teksten
+
   p «Beowulf» er et angelsaksisk heltekvad. Kvadet regnes for å være det engelske  nasjonaleposet og er det viktigste enkeltverket fra angelsaksisk tid (om lag 400–1066).
 
   p Forskerne mener at teksten først ble overlevert muntlig, og at den er skrevet ned mellom 700 og 1000. Dikteren er ukjent. 
@@ -10,14 +17,26 @@ PageBook
 
   p Det finnes dessuten to moderne filmversjoner av Beowulf. 
 
+  figure.col.col6.left-image
+    img(src="./Beowulf_film.jpg" alt="Det finst også to moderne filmversjoner av Beowulf.")
+    figcaption Det finst også to moderne filmversjoner av Beowulf.
+
   h3 Innhold
   p Handlingen i eposet er lagt til Danmark og Sverige i urnordisk tid. Uhyret Grendel herjer i riket til danskekongen Hrothgar. Hver natt hjemsøker Grendel kongshallen og dreper kongens menn. Da drar helten Beowulf ut for å hjelpe Hrothgar. Han bekjemper først Grendel og deretter mor hans, som prøver å hevne sønnen. 
 
   p Seinere blir Beowulf konge i hjemlandet sitt, Gautland (kanskje dagens Götaland i Sør-Sverige). Siste del av kvadet skildrer hvordan han drar ut sammen med mennene sine for å drepe en drake som ødelegger landet hans. Men det er bare én av mennene som tør å møte draken sammen med han. Beowulf greier å ta livet av draken, men blir selv drept. Han blir så hauglagt av mennene sine. 
 
+  figure.col.col4.left-image
+    img(src="./Beowulf_Gautland.jpg" alt="Senere blir Beowulf konge i hjemlandet sitt, Gautland")
+    figcaption Senere blir Beowulf konge i hjemlandet sitt, Gautland
+
   p Samfunnet som skildres i eposet, legger stor vekt på egenskaper som æresfølelse og mot. Kongens oppgave er å forsvare landet. Derfor trenger han menn som gjør krigstjeneste for han, og han lønner dem med våpen, rikdommer og land. Krigerne er høyt verdsatt i denne typen samfunn. 
 
   p Helten Beowulf er preget av en sterk skjebnetro. Han er overbevist om at det er skjebnen som bestemmer når han skal dø. Denne skjebnetroen er dypt forankret i germansk krigertradisjon. Vi finner den samme troen også i de norrøne heltediktene og i islendingesagaene. 
+  
+  figure.col.col6.right-image
+    img(src="./Beowulf_i_kamp_med_draken.jpg" alt="Beowulf i kamp med dragen. Til høyre gravhaugen i Skalunda i Götaland der Beowulf skal være hauglagt.")
+    figcaption Beowulf i kamp med dragen. Til høyre gravhaugen i Skalunda i Götaland der Beowulf skal være hauglagt.
 
   h3 Heltedikt 
   p Alle stammer og nasjoner har sagn og myter som bekrefter hvem de er, hvor de kommer fra, og formidler tenkemåter og verdier som er viktige for dem. 
@@ -26,13 +45,19 @@ PageBook
 
   p Vi regner de to eposene til den greske dikteren Homer for den eldste heltediktningen i Europa. I «Iliaden» skildrer Homer kampen om Troja, mens «Odysseen» handler om Odyssevs’ farefulle hjemreise fra Troja.
 
+  figure.col.col6.right-image
+    img(src="./Beowulf_Prakthjelm.jpg" alt="Prakthjelm i jern og bronse funnet i en svensk båtgrav fra den tida da Beowulf levde.")
+    figcaption Prakthjelm i jern og bronse funnet i en svensk båtgrav fra den tida da Beowulf levde.
+
   p Romerne har sitt nasjonalepos i «Æneiden», skrevet av dikteren Vergil. Eposet handler om helten Æneas, som flykter fra Troja og seinere blir stamfar til romerne. 
 
   p Fra og med 900-tallet blir mange europeiske heltedikt skrevet ned. Samtidig blir de utvidet til omfattende helteepos etter mønster av sine antikke forbilder, med brede skildringer og tallrike parallellhandlinger. 
 
   p De mest kjente europeiske heltediktene i tillegg til «Beowulf «er det franske «Rolandskvadet» fra 1000-tallet, det spanske heltediktet om El Cid, som ble skapt rundt 1140, og «Nibelungen-eposet» fra tidlig på 1200-tallet.   
 
-
+  figure
+    img(src="./Beowulf_Heltedikt.jpg" alt="Eposet handler om helten Æneas som flykter fra Troja og senere blir stamfar til romerne.")
+    figcaption Eposet handler om helten Æneas som flykter fra Troja og senere blir stamfar til romerne.
 
 
 </template>
--- src/books/beowulf/BeowulfNN.vue
+++ src/books/beowulf/BeowulfNN.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
-PageBook
+PageBook(:showAuthor="false")
+
+  figure.col.col6.right-image
+    img(src="./Beowulf_utklipp.jpg" alt="Første side av ”Beowulf”-kvadet i eit manuskript skrive ein gong mellom 700 og 1000")
+    figcaption Første side av ”Beowulf”-kvadet i eit manuskript skrive ein gong mellom 700 og 1000
+      NdlaAudio(source="media/books/beowulf/beowulf_sitat_britishlibary.mp3")
+      a(href="http://www.heimskringla.no/wiki/Beowulf") Les teksten
+
   p «Beowulf» er eit angelsaksisk heltekvad. Kvadet blir rekna for å vere det engelske nasjonaleposet og er det viktigaste enkeltverket frå angelsaksisk tid (om lag 400–1066). 
 
   p Forskarane meiner at teksten først er overlevert munnleg, og at han er skriven ned mellom 700 og 1000. Diktaren er ukjend. 
@@ -10,14 +17,26 @@ PageBook
 
   p Det finst òg to moderne filmversjonar av Beowulf.
 
+  figure.col.col6.left-image
+    img(src="./Beowulf_film.jpg" alt="Det finst òg to moderne filmversjonar av Beowulf.")
+    figcaption Det finst òg to moderne filmversjonar av Beowulf.
+
   h3 Innhald
   p Handlinga i eposet er lagd til Danmark og Sverige i urnordisk tid. Uhyret Grendel herjar i riket til danskekongen Hrothgar. Kvar natt heimsøkjer Grendel kongshallen og drep mennene til kongen. Da dreg helten Beowulf ut for å hjelpe Hrothgar. Han nedkjempar først Grendel og deretter mor hans, som prøver å hemne sonen.
 
   p Seinare blir Beowulf konge i heimlandet sitt, Gautland (kanskje dagens Götaland i Sør Sverige). Siste del av kvadet skildrar korleis han dreg ut saman med mennene sine for å drepe ein drake som legg landet hans i øyde. Men det er berre éin av mennene som torer møte draken saman med han. Beowulf maktar å ta livet av draken, men blir sjølv drepen. Han blir så hauglagd av mennene sine.
 
+  figure.col.col4.left-image
+    img(src="./Beowulf_Gautland.jpg" alt="Seinare blir Beowulf konge i heimlandet sitt, Gautland")
+    figcaption Seinare blir Beowulf konge i heimlandet sitt, Gautland
+
   p Samfunnet som blir skildra i eposet, legg stor vekt på eigenskapar som ærekjensle og mot. Oppgåva til kongen er å forsvare landet. Derfor treng han menn som gjer krigsteneste for han, og han lønner mennene sine med våpen, rikdommar og land. Krigarane er høgt verdsette i denne typen samfunn. 
 
   p Helten Beowulf er prega av ei sterk lagnadstru. Han er overtydd om at det er lagnaden som avgjer når han skal døy. Denne lagnadstrua er djupt forankra i germansk krigartradisjon. Vi finn den same trua også i dei norrøne heltedikta og i islendingesogene. 
+
+  figure.col.col6.right-image
+    img(src="./Beowulf_i_kamp_med_draken.jpg" alt="Beowulf i kamp med draken. Til høgre gravhaugen i Skalunda i Götaland der Beowulf skal vere hauglagd.")
+    figcaption Beowulf i kamp med draken. Til høgre gravhaugen i Skalunda i Götaland der Beowulf skal vere hauglagd.
 
   h3 Heltedikt
   p Alle stammar og nasjonar har segner og mytar som stadfestar kven dei er, kvar dei kjem frå, og formidlar tenkjemåtar og verdiar som er viktige for dei.
@@ -26,11 +45,20 @@ PageBook
 
   p Vi reknar dei to eposa til den greske diktaren Homer for den eldste heltediktinga i Europa. I ”Iliaden” skildrar Homer kampen om Troja, mens ”Odysseen” handlar om den farefulle heimreisa til Odyssevs frå Troja.
 
+  figure.col.col6.right-image
+    img(src="./Beowulf_Prakthjelm.jpg" alt="Prakthjelm i jarn og bronse funnen i ei svensk båtgrav frå den tida då Beowulf levde.")
+    figcaption Prakthjelm i jarn og bronse funnen i ei svensk båtgrav frå den tida då Beowulf levde.
+
   p Romarane har sitt nasjonalepos i ”Æneiden”, skrive av diktaren Vergil. Eposet handlar om helten Æneas som flyktar frå Troja og seinare blir stamfar til romarane.
 
   p Frå 900-talet blir mange europeiske heltedikt skrivne ned. Samstundes blir dei utvida til omfattande helteepos etter mønster av dei antikke førebileta, med breie skildringar og talrike parallellhandlingar.
 
   p Dei mest kjende europeiske heltedikta i tillegg til ”Beowulf” er det franske ”Rolandskvadet” frå 1000-talet, det spanske heltediktet om El Cid, som blei skapt rundt 1140, og ”Nibelungen-eposet” frå tidleg på 1200-talet.
+
+  figure
+    img(src="./Beowulf_Heltedikt.jpg" alt="Eposet handlar om helten Æneas som flyktar frå Troja og seinare blir stamfar til romarane.")
+    figcaption Eposet handlar om helten Æneas som flyktar frå Troja og seinare blir stamfar til romarane.
+
 
 
 </template>
