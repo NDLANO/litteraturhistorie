7fd56c6d18c2500afca0863e6cf52aa1a525f436
Roger Hestad <roger@doublethink.no>
2021-05-31 09:59:40 +0200

    Copy of old implementation of AsbjornsenMoe which contains some examples of sound/video etc.

---
+++ src/books/asbjornsenmoe/AsbjornsenMoeNN copy.vue
@@ -0,0 +1,129 @@
+<template lang="pug">
+PageBook(:bannerImage="require(`./banner.jpg`)")
+  h3 Draumen om  det norske
+
+  p Eksempel på link til <router-link to="/nb/periods/barokk">Barokk-perioden</router-link>
+
+  NdlaAudio(source="media/books/asbjornsenmoe/test.mp3")
+
+  p I 1814 får Noreg sjølvråderett og eiga grunnlov. Samtidig veks interessa for alt som kan kallast opphavleg norsk. Men Noreg har vore under Danmark i fleire hundre år, og i heile denne perioden har norske forfattarar skrive på dansk. Ved inngangen til 1800talet finst det derfor ingen norskspråkleg litteratur bortsett frå eventyr, viser og segner som var over­leverte munnleg frå generasjon til genera­sjon. 
+
+  NdlaVideo(source="media/books/asbjornsenmoe/BigBuckBunny.mp4")
+
+  h3 Større interesse for folkelitteraturen i Europa
+
+  p Den dominerande kulturelle og åndelege retninga i Europa i første del av 1800­talet er romantikken. Romantikara­ne gjer opprør mot fornuftsdyrkinga som har prega 1700­talet. Vitskap og sivilisa­sjon har øydelagt menneskesinnet, hevdar filosofen JeanJacques Rousseau, det gjeld å finne tilbake til det naturlege og ekte. Derfor byrjar romantikarane mellom anna å interessere seg for den folkelege kulturen.
+
+  p I tillegg meiner romantikarane at folke litteraturen er eit uttrykk for folke­sjela, den kulturelle og historiske bak­grunnen som er felles for ein nasjon. Utover på 1800­talet blir det samla inn og skrive ned eventyr, segner og folkeviser over heile Europa. Allereie i 1812 gir brør­ne Jacob og Wilhelm Grimm ut den første eventyrsamlinga si i Tyskland.
+
+  h3 Noreg følgjer etter
+
+  p I 1830­åra slår den romantiske bølgja inn over Noreg for fullt. Koplinga mel­lom den nyvekte interessa for det norske og dei romantiske førestellingane om det folkelege som noko opphavleg og ekte, blir i Noreg omtalt som nasjonalromantikk. Prosten Andreas Faye gir ut den første samlinga av norske segner i 1833. I 1840­åra kjem så Peter Christen Asbjørnsen og Jørgen Moe med si første samling av nor­ske eventyr og segner. I tillegg er det fleire andre som samlar inn eventyr, segner og viser i Noreg.
+
+  img(src="./asbjornsenmoe_01.jpg" alt="Maleriet Soria Moria av Tideman og Gude e.l.")  
+
+  h3 Asbjørnsen og Moe
+
+  p Dei to unggutane Peter Christen Asbjørnsen frå Christiania og Jørgen Moe frå Hole i Buskerud går saman på Norderhov skole vinteren 1826. Her vaknar  interessa for norsk folkekultur. Dei reiser rundt på bygdene, snakkar med folk og skriv ned eventyr og segner frå ulike bygde lag. Nedskrivingane frå desse reisene resulterer etter kvart i fleire samlingar med norske eventyr og segner.
+
+  p 
+    img(src="./asbjornsenmoe_02.jpg" alt="Illustrasjon av Asbjørnsen" align="left" class="left-image")
+    | Asbjørnsen er først og fremst folkeminnesamlar. Han har mykje til overs for den direkte og ofte  humoristiske forteljemåten til vanlege folk. I bøkene hans får vi levande skildringar av kvardagsmenneske som eventyrsamlaren har møtt rundt om på reisene sine. 
+  
+  br
+  p 
+    img(src="./asbjornsenmoe_03.jpg" alt="Illustrasjon av Moe" align="left" class="left-image")
+    | Jørgen Moe er prest i Krødsherad og i Drammen, seinare biskop i Kristiansand. Han er mest oppteken av religiøse folkeeventyr og legender. Også han gir ut eigne dikt og forteljingar, mellom anna den kjende barneboka <em>I Brønden og i Kjærnet</em>.
+
+  h3 Norske folkeeventyr
+
+  p Den første eventyrsamlinga til Asbjørnsen og Moe, <em>Norske Folkeeventyr</em>, blir utgitt som fire hefte. Det første heftet kjem i 1841, det siste i 1844. Seinare gir Asbjørnsen ut fleire eventyrsamlingar på eiga hand, mellom anna <em>Norske Huldreeventyr</em> og <em>Folkesagn</em> (1845–1847).
+
+  img(src="./asbjornsenmoe_04.jpg" alt="Illustrasjon fra eventyr")
+
+  p Eventyra kan delast inn i grupper etter innhaldet. I samlinga Norske Folkeeventyr finn vi for eksempel desse undersjangrane:
+    ul
+      li 
+        <strong>Dyreeventyr</strong> som «Reveenka», «De tre bukkene Bruse» og «Hanen og høna»: Hovudpersonane er dyr som blir tillagde spesielle eigenskapar. Bjørnen er for eksempel som oftast sterk, men dum, mens reven er ein luring.
+      li 
+        <strong>Skjemteeventyr</strong> som «Pannekaka», «Mannen som skulle stelle heime» og «Presten og klokkeren»: Desse eventyra har ofte ei satirisk form og hengjer ut negative eigenskapar som det å vere grådig, dum eller innbilsk.
+      li 
+        <strong>Legendeeventyr</strong> om «Gjertrudsfuglen» og «Jomfru Maria og svala»: Dette er religiøse forteljingar der hovudpersonen er jomfru Maria, St. Peter, HeilagOlav eller Vårherre. Mange av forteljingane illustrerer religiøse eller moralske sanningar.
+      li
+        <strong>Undereventyr</strong> som eventyra om «Askeladden og de gode hjelperne» og «Kvitebjørn Kong Valamon»: I desse eventyra møter vi både troll og tussar, menneske og dyr, kongar og prinsesser. Det magiske har sin sjølvskrivne plass her.
+      li
+        <strong>Novelleeventyr</strong> som «Prinsessa som ingen kunne målbinde»: I novelleeventyra spelar det overnaturlege lita eller inga rolle.
+  
+  h3 Eventyrsjangeren
+
+  p <em>Eventyr</em> er forteljingar med oppdikta innhald som er overleverte munnleg frå generasjon til generasjon. Dei har verken tid eller stadfesting, men handlinga er like vel plassert i ein kulturhistorisk saman heng som vi kan kjenne att. Vi møter det gamle norske bondesamfunnet med kongar og prinsesser, prestar, futar, klokkarar, bønder og tenestefolk.
+
+  p Dei fleste eventyra følgjer eit enkelt grunnmønster: Helten møter ei utfordring eller eit problem som skal løysast. Slutten er aldri noko å lure på: Helten lykkast, og motstandarane går det dårleg med. Spenninga ligg i kva helten må gjere for å vinne over motstandarane. Handlinga blir alltid fortald kronologisk, utan frampeik eller tilbakeblikk. 
+
+  p Karakterane i eventyret er eindimensjonale og statiske. Dei er anten gode eller slemme, og dei endrar seg aldri. Eit anna typisk sjangertrekk ved eventyra er bruken av faste formlar som «det var en gang» og «snipp, snapp, snute». Gjen taking blir gjerne brukt for å auke spenninga: Det er ikkje første eller andre  gongen, men tredje gongen som gjeld. 
+
+  p Eventyr finst i alle kulturar, og ofte går dei same historiene att i litt ulik form i ulike land. Eventyret om «Kvitebjørn Kong Valamon» er ein norsk versjon av det eventyret som Disney-filmen «Skjønnheten og udyret» byggjer på. 
+
+  img(src="./asbjornsenmoe_05.jpg" alt="Prinsessen og isbjørnen")
+
+
+  h3 Folkedikting
+
+  p Samlingane til Asbjørnsen og Moe inneheld ikkje berre eventyr, men også fleire segner, for eksempel «Mannen som gikk i vargham» og «Tuftefolket på Sandflesa». 
+
+  img(src="./asbjornsenmoe_06.jpg" alt="Illustrasjon fra eventyr")
+
+  p Segna skil seg frå eventyret ved at segna gjerne er knytt til ein bestemt stad og ei viss tid. Segna gir ofte overnaturlege forklaringar på naturfenomen eller historiske hendingar.
+
+  img(src="./asbjornsenmoe_07.jpg" alt="Danser i skogen")
+
+  p Somme gonger lagar Asbjørnsen og Moe ei såkalla rammeforteljing rundt eventyra eller segnene. Rammeforteljinga handlar om korleis dei to folkeminnegranskarane reiser rundt på Austlandet og møter ulike forteljarar. På den måten kan dei også skildre kva slags personar forteljarane er, og korleis dei fortel historiene sine. Eksempel på slike rammeforteljingar er «En natt i Nordmarka» og «En sommernatt på Krokskogen».
+
+  h3 Sjangertrekk og dramaturgi i  «De tre mostrene»
+
+  p Eventyret om dei tre mostrene inneheld fleire sjangertrekk som er typiske for eventyret:
+    ul
+      li Forteljinga er ikkje tid- eller stadfesta, og personane er eindimensjonale og karikerte. Spenninga knyter seg til om hovudpersonen klarer å løyse det oppdraget ho får. I dette tilfelle får jenta hjelp av tre kvinner med overnaturlege evner. Som lønn må ho love å invitere dei i bryllaupet sitt.
+
+      li Eventyret opnar med formelen «Det var en gang», som straks vekkjer heilt klare sjangerforventningar hos lesaren. Dramaturgien utnyttar den såkalla tretalslova: Jenta får tre oppdrag ho må løyse, og møter tre hjelparar. Samtalane mellom henne og hjelparane inneheld dei same replikkane tre gonger på rad. Gjentakingane skaper ein eigen rytme i teksten.
+
+      li Og som dei fleste eventyra gjer, endar også dette godt til slutt. Jenta slepp unna dei arbeidsoppgåvene dei misunnelege kollegaene ville prakke på henne, og lever lykkeleg med prinsen alle sine dagar.
+  
+  h3 Språket hos Asbjørnsen og Moe
+
+  p Eventyr og segner er munnleg littera-tur fortald frå generasjon til generasjon. Dei som tek på seg å skrive ned desse tekstane, står derfor overfor ei utfordring: Korleis kan ein ta vare på det munnlege i skriftleg form? Resultatet er at eventyr-språket har fått ein munnleg setnings-bygnad med mange jamstilte setningar og eit stort innslag av replikkar.
+
+  p Når Asbjørnsen og Moe går i gang med å skrive ned norske folkeeventyr og segner, finst det framleis ikkje noko eige norsk skriftspråk. Eventyra blir derfor skrivne ned på dansk. Men Asbjørnsen og Moe prøver å ta omsyn til den munnlege originalteksten og nyttar mange norske dialektord.
+
+  p Gjennom det språklege arbeidet med eventyra er Asbjørnsen og Moe med på å rydde grunnen for fornorskingsprogram-met til språkmannen Knud Knudsen, og for så vidt også for arbeidet til Ivar Aasen med å reise eit nytt norsk skriftspråk med utgangspunkt i norske dialektar.
+
+  p
+    img(src="./asbjornsenmoe_08.jpg" alt="Gammelt bokomslag")
+
+  br
+  
+  p Video under er et eksempel på integrering av video fra NRK Skole
+  NrkVideo(id="3819")
+
+  br
+
+</template>
+
+<script>
+import PageBook from "@/components/PageBook";
+import NdlaVideo from "@/components/NdlaVideo";
+import NdlaAudio from "@/components/NdlaAudio";
+import NrkVideo from "@/components/NrkVideo";
+
+export default {
+  name: "Book",
+  components: {
+    NdlaAudio,
+    NdlaVideo,
+    NrkVideo,
+    PageBook,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
