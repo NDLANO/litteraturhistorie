027e7d1718113428e618a9994daa21ecb900f564
Roger Hestad <roger@wodger.com>
2021-06-20 12:05:48 +0200

    Timeline: Add a ref for each menu item, type menuItem0, menuItem1, etc

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -16,8 +16,8 @@
       <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" style="width: 24px; height:24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg> 
     .mainMenu_title Meny
     ul.mainMenu_list
-      li.mainMenu_list_item(v-for="period in periods" :key="period.id")        
-        a(href="#" @click="onNavButtonClick(period.id)") {{ period.nbTitle}}  
+      li.mainMenu_list_item(v-for="(period, index) in periods" :key="period.id")        
+        a(:ref="'menuItem' + index" href="#" @click="onNavButtonClick(period.id)" tabindex="1") {{ period.nbTitle}}  
   
   // * HEADER
   .lo_topBar
