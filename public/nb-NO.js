f926aa6aac62520d8c837a6b32f21a4c0176c1dd
Roger Hestad <roger@wodger.com>
2021-06-20 13:22:39 +0200

    Adds show books/persons text to language files

--- public/nb-NO.js
+++ public/nb-NO.js
@@ -2,6 +2,8 @@ window.litteraturhistorieDictionaryNb = {
   general: {
     title: "NDLA bokmål Litteraturhistorie",
     pageBookBackButton: "Tilbake til tidslinjen",
+    showBooks: "Vis bøker",
+    showPersons: "Vis sentrale personer",
   },
   audioplayer: {
     showText: "Vis tekst",
--- public/nn-NO.js
+++ public/nn-NO.js
@@ -2,6 +2,8 @@ window.litteraturhistorieDictionaryNn = {
   general: {
     title: "NDLA nynorsk Litteraturhistorie",
     pageBookBackButton: "Tilbake til tidslinja",
+    showBooks: "Vis bøker",
+    showPersons: "Vis sentrale personar",
   },
   audioplayer: {
     showText: "Vis tekst",
