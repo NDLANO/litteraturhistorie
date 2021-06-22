cac7af97142da6d39ea49dcf6095c53fe4ef9953
Roger Hestad <roger@wodger.com>
2021-06-22 11:59:33 +0200

    Timeline: Implements langCode for author texts

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -6,7 +6,7 @@
         <svg @click="showAuthorModal = false" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" style="width: 24px; height:24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg> 
       .authorPopupContent
         h2 {{selectedAuthor.name}} <br>({{selectedAuthor.from}}–{{selectedAuthor.to}})
-        ul(v-html="selectedAuthor.nbText")
+        ul(v-html="selectedAuthor[globalVars.langCode + 'Text']")
 // * CONTENT
 .lo_globalContainer
   
