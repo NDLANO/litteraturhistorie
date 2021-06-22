5c983da37c383adec83550a2e83e78e9f27d206d
Roger Hestad <roger@wodger.com>
2021-06-22 12:45:06 +0200

    NdlaVideo: Adds support for captions track

--- src/components/NdlaVideo.vue
+++ src/components/NdlaVideo.vue
@@ -2,6 +2,12 @@
 div
   video(ref="player" v-if="playerType === 'local'" playsinline controls :data-poster="poster")
     source(:src="source")
+    track(
+      v-if="captionsUrl!==''" 
+      kind="captions"
+      :src="captionsUrl"
+      srcLang="nb"
+      )
   
   .div.plyr__video-embed(id="ytplayer" v-if="playerType === 'youtube'" ref="ytplayer")
     iframe(:src="source" allowfullscreen allowtransparancy allow="autoplay")
@@ -17,6 +23,10 @@ export default {
     source: {
       type: String,
       required: true,
+    },
+    captionsUrl: {
+      type: String,
+      default: "",
     },
     description: {
       type: String,
