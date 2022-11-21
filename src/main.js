806eb4a0a7bb1438744c8628bbb86f3fa65e63f5
janlindso <jan.frode.lindso@gmail.com>
2022-11-21 13:47:23 +0100

    Added gtag

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createI18n } from "vue-i18n";
+import VueGtag from "vue-gtag";
 
 const i18n = createI18n({
   locale: "no",
 });
-
 const app = createApp(App);
 app.config.devtools = true;
 app.use(router);
 app.use(i18n);
+app.use(VueGtag, {
+  config: { id: "G-H7W6XF0WSR" },
+});
 app.mount("#app");
