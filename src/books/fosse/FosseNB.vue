abfb2aef12a33bf7638a0c8a3b049a9d19f23d04
Gunnar Velle <gunnar.velle@gmail.com>
2025-08-20 13:06:02 +0200

    Fix whitespacehandling for Fosse

--- src/books/fosse/FosseNB.vue
+++ src/books/fosse/FosseNB.vue
@@ -7,23 +7,11 @@ PageBook
     img(src='./fosse_portrett1.jpg' alt='Svart hvitt portrett av mann med grått hår og skjegg som har hånda på haka og ser ned til høyre. Foto.')
     figcaption Foto: Tom A. Kolstad / Aftenposten / Scanpix
 
-  p Jon Fosse (1959– ) debuterte i 1985 med romanen
-    em Raudt, svart
-    | og fikk sitt store gjennombrudd som forfatter på begynnelsen av 1990-tallet. Med skuespill som
-    em Og aldri skal vi skiljast
-    | (1993),
-    em Namnet
-    | (1995) og
-    em Nokon kjem til å komme
-    | (1999) ble Jon Fosse den første norske dramatikeren siden Ibsen med betydelig internasjonal suksess. Fosse blir da også ofte omtalt som «den nye Ibsen», og felles for de to dramatikerne er – i tillegg til internasjonal suksess – en nyskapende og utforskende teaterteknikk.
+  p Jon Fosse (1959– ) debuterte i 1985 med romanen #[em Raudt, svart] og fikk sitt store gjennombrudd som forfatter på begynnelsen av 1990-tallet. Med skuespill som #[em Og aldri skal vi skiljast] (1993), #[em Namnet] (1995) og #[em Nokon kjem til å komme] (1999) ble Jon Fosse den første norske dramatikeren siden Ibsen med betydelig internasjonal suksess. Fosse blir da også ofte omtalt som «den nye Ibsen», og felles for de to dramatikerne er – i tillegg til internasjonal suksess – en nyskapende og utforskende teaterteknikk.
 
   p Jon Fosses forfatterskap er allerede av stort omfang – og han har skrevet i både roman-, drama- og lyrikksjangeren. Jon Fosse har mottatt flere priser og utmerkelser. Han er æresmedlem av Norske Dramatikeres Forbund, utnevnt til kommandør av St. Olavs orden og mottok i 2005 Brageprisens hederspris. I oktober 2023 ble han tildelt Nobels litteraturpris, som den første nynorskforfatteren noensinne.
 
-  p I tillegg til den skjønnlitterære produksjonen har Fosse gitt ut flere essay- og artikkelsamlinger. Han er utdannet cand. philol. med litteraturvitenskap som hovedfag, og i essay- og artikkelsamlingene sine undersøker han både egen og andres litteratur i et teoretisk perspektiv. I perioden 1987–1993 underviste Fosse på Skrivekunstakademiet i Hordaland, og han har – sammen med Jan Kjærstad – vært redaktør for tidsskriftet Bøk. Fosse står også bak flere oversettelser og gjenfortellinger av klassiske verk, som for eksempel Johann Wolfgang von Goethes (1749–1832)
-    em Faust
-    | (1808 og 1832) og Harold Pinters (1930–2008)
-    em Ashes to ashes
-    | (1996).
+  p I tillegg til den skjønnlitterære produksjonen har Fosse gitt ut flere essay- og artikkelsamlinger. Han er utdannet cand. philol. med litteraturvitenskap som hovedfag, og i essay- og artikkelsamlingene sine undersøker han både egen og andres litteratur i et teoretisk perspektiv. I perioden 1987–1993 underviste Fosse på Skrivekunstakademiet i Hordaland, og han har – sammen med Jan Kjærstad – vært redaktør for tidsskriftet Bøk. Fosse står også bak flere oversettelser og gjenfortellinger av klassiske verk, som for eksempel Johann Wolfgang von Goethes (1749–1832) #[em Faust] (1808 og 1832) og Harold Pinters (1930–2008) #[em Ashes to ashes] (1996).
 
   h3 Verdenskjent vestlandsforfatter
 
@@ -32,16 +20,13 @@ PageBook
     figcaption Foto: Bård Løken / Samfoto  / Scanpix
 
 
-  p Jon Fosse har selv karakterisert seg som en
-    em vestlandsforfatter
-    | . Ofte legger han den konkrete handlingen i tekstene sine til avsidesliggende steder på Vestlandet – i et naust nede ved fjorden eller i et forlatt hus innerst i dalen. Den karrige og harde vestlandsnaturen danner et viktig bakteppe i diktningen hans. Fosse sier selv at det er «himmelen, vinden og lyngen» som inspirerer han skrivingen. Han forsøker å lytte til både stillheten og lydene i naturen, og forsøker å formidle følelsene de gir, i tekstene sine. Særlig regn er et naturfenomen som ofte går igjen i Fosses diktning.
+  p Jon Fosse har selv karakterisert seg som en #[em vestlandsforfatter]. Ofte legger han den konkrete handlingen i tekstene sine til avsidesliggende steder på Vestlandet – i et naust nede ved fjorden eller i et forlatt hus innerst i dalen. Den karrige og harde vestlandsnaturen danner et viktig bakteppe i diktningen hans. Fosse sier selv at det er «himmelen, vinden og lyngen» som inspirerer han skrivingen. Han forsøker å lytte til både stillheten og lydene i naturen, og forsøker å formidle følelsene de gir, i tekstene sine. Særlig regn er et naturfenomen som ofte går igjen i Fosses diktning.
 
   p Men også mentaliteten og kulturen som skildres, er ifølge Fosse typisk vestlandsk. Dialogen mellom menneskene er knapp, humoren underfundig, og hendelser blir ofte underdrevet. Tekstene er preget av en nøktern tone der språket er renset for patos og overflod.
 
   figure.col.col12
     img(src='./Nokon-kjem-til-a-komme_Bildet-av-Fosse-under-Verdenskjent-forfatter.jpg' alt='Portrett av mann som har hånda på haka og ser rett inn i kamera. Foto.')
     //- figcaption Foto: Trygve Indrelid / Aftenposten / Scanpix
-
 
   p Selv om både miljø og karakterer i litteraturen til Fosse kan oppfattes som uttrykk for en regional og kanskje litt sær norsk mentalitet, framstår karakterene som svært allmennmenneskelige – noe som kanskje er svaret på hvorfor Fosse slår så godt an internasjonalt. Tekstene er også ofte fylt av vemod og melankoli. Fosse sier selv av livet er grunnleggende trist. Alt du har kjært i livet, og du  selv, vil etter hvert forsvinne og bli borte. Denne vissheten om at livet er i stadig oppløsning og forvitring, er en allmennmenneskelig følelse. I Fosses litteratur kommer dette til uttrykk gjennom en indre uro og karakterer som er både skjøre og voldsomme på  samme tid.
 
@@ -50,33 +35,15 @@ PageBook
   p Denne religiøse interessen deler Fosse med flere av samtidens forfattere. Forfattere som Jonny Halberg (1962– ), Dag Solstad (1941– ) og Karl Ove Knausgård (1968– ) har alle skrevet tekster med klare bibelske referanser, og i likhet med Jon Fosse er de opptatt av hvordan religion preger mennesker i det postmoderne samfunnet. Filosofen Jürgen Habermas (1929– ) har påpekt at dagens samfunn på mange måter kan kalles et postsekulært samfunn – et samfunn der religion har fått fornyet interesse – både politisk, kulturelt og individuelt. Hos Fosse må religion forstås både som en individuell lengsel og en kulturell forankring.
 
   h3 «Pausenes mester»
-  p Jon Fosse ble på 1990-tallet kjent som en nyskapende lyriker i norsk samtidslitteratur. Fosse gir selv uttrykk for at det er lyrikken som ligger han nærmest; her utfordrer han seg selv både som menneske og forfatter. I 1986 gav han ut samlingen
-    em Engel med vatn i augene
-    | . På 90-tallet gav han ut flere samlinger, som
-    em Hund og engel
-    | (1992) og
-    em Nye dikt
-    | (1997).
+  p Jon Fosse ble på 1990-tallet kjent som en nyskapende lyriker i norsk samtidslitteratur. Fosse gir selv uttrykk for at det er lyrikken som ligger han nærmest; her utfordrer han seg selv både som menneske og forfatter. I 1986 gav han ut samlingen #[em Engel med vatn i augene]. På 90-tallet gav han ut flere samlinger, som #[em Hund og engel](1992) og #[em Nye dikt] (1997).
 
-  p Fosses dikt kretser ofte rundt de samme språklige bildene og symbolene, særlig opptatt er han av hunder, engler og sjøen. I
-    em Nye dikt
-    | ser vi tydelig både Fosses særegne språklige eksperiment og hvordan han kretser rundt religiøse tema og spørsmål. Flere av diktene er blant annet utformet som bønner, og Fosse har en rekke bibelske allusjoner i diktene sine.
+  p Fosses dikt kretser ofte rundt de samme språklige bildene og symbolene, særlig opptatt er han av hunder, engler og sjøen. I #[em Nye dikt] ser vi tydelig både Fosses særegne språklige eksperiment og hvordan han kretser rundt religiøse tema og spørsmål. Flere av diktene er blant annet utformet som bønner, og Fosse har en rekke bibelske allusjoner i diktene sine.
 
-  p Fosse har også skrevet flere romaner. I tobindsromanen
-    em Melancholia I–II
-    | (1995–1996) skriver han en suggererende og gjentakende fortelling ut fra maleren Lars Hertervigs (1830–1902) perspektiv. I romanen skildres en melankolsk kunstner med sterkt skiftende humør. Fortellemåten er langsom og gjentakende, og den skaper i romanen både en poetisk grunntone og en nærmest manisk suggererende form.
+  p Fosse har også skrevet flere romaner. I tobindsromanen #[em Melancholia I–II] (1995–1996) skriver han en suggererende og gjentakende fortelling ut fra maleren Lars Hertervigs (1830–1902) perspektiv. I romanen skildres en melankolsk kunstner med sterkt skiftende humør. Fortellemåten er langsom og gjentakende, og den skaper i romanen både en poetisk grunntone og en nærmest manisk suggererende form.
 
-  p I romanen
-    em Stengd gitar
-    | (1985) møter vi ei mor som ved et uhell låser seg ute fra leiligheten sin. Inne i leiligheten sitter hennes ett år gamle sønn alene, og romanen handler om hvordan mora hvileløst vandrer rundt etter hjelp til å komme seg inn. Ferden og tankene tar mora stadig lenger vekk fra leiligheten, og leseren får aldri noe svar på hvordan det går med barnet. Romanen slutter slik den begynner – med mora som visker gjennom  døra: «Ungen min, eg er her no. Ikkje ver redd, seier eg». Leseren sitter igjen med en grunnleggende uro.
+  p I romanen #[em Stengd gitar] (1985) møter vi ei mor som ved et uhell låser seg ute fra leiligheten sin. Inne i leiligheten sitter hennes ett år gamle sønn alene, og romanen handler om hvordan mora hvileløst vandrer rundt etter hjelp til å komme seg inn. Ferden og tankene tar mora stadig lenger vekk fra leiligheten, og leseren får aldri noe svar på hvordan det går med barnet. Romanen slutter slik den begynner – med mora som visker gjennom  døra: «Ungen min, eg er her no. Ikkje ver redd, seier eg». Leseren sitter igjen med en grunnleggende uro.
 
-  p Den grunnleggende uroen og underliggende angsten vi ofte kan finne i romanene til Fosse, er – nærmest naturlig nok – tonet noe ned i barnebøkene hans. I boka
-    em Kant
-    | (1990) møter vi en åtte år gammel gutt som sammen med faren filosoferer over de store spørsmålene i livet. Men også bøker som
-    em Uendeleg seint
-    | (1989) og
-    em Vått og svart
-    | (1994) handler om barn som gjennom samtaler forsøker å sette ord på hva som er vanskelig, og på den måten bearbeider kimer til angst og uro.
+  p Den grunnleggende uroen og underliggende angsten vi ofte kan finne i romanene til Fosse, er – nærmest naturlig nok – tonet noe ned i barnebøkene hans. I boka #[em Kant] (1990) møter vi en åtte år gammel gutt som sammen med faren filosoferer over de store spørsmålene i livet. Men også bøker som #[em Uendeleg seint] (1989) og #[em Vått og svart] (1994) handler om barn som gjennom samtaler forsøker å sette ord på hva som er vanskelig, og på den måten bearbeider kimer til angst og uro.
 
   p Både tematisk og språklig har Fosse tatt med seg sin særegne stil fra lyrikk- og prosatekstene til dramasjangeren. Siden debuten på Den Nasjonale Scene i Bergen i 1994 har han skrevet hele 22 skuespill – en dramatisk raptus på lik linje med Ludvig Holbergs. Fosses dramatikk blir av mange omtalt som et paradigmeskifte innen norsk teater- og skuespilltradisjon. Stykkene tvinger oss til å tenke på skuespill og teater på en grunnleggende ny måte. Fosse blir gjerne kalt «pausenes mester», og det er nettopp fraværet av dialoger og replikker som gjør stykkene hans så unike og nyskapende.
 
@@ -88,30 +55,18 @@ PageBook
   //-     br
   //-     | Foto: Helge Hansen / Aftenposten / Scanpix
 
-  p I 1996 gav Jon Fosse ut
-    em Nokon kjem til å komme
-    | . Stykket handler om ei navnløs kvinne og en navnløs mann som kjøper seg et hus nede ved sjøen. «Ho» og «Han» ønsker å bo «aleine i kvarandre», men den «tosomme ensomheten» de lever i, blir utfordret da husets tidligere eier involverer seg i livene deres. Den tidligere huseierens nærvær vekker sjalusi, usikkerhet og voldsomme følelser mellom de to hovedkarakterene.
+  p I 1996 gav Jon Fosse ut #[em Nokon kjem til å komme]. Stykket handler om ei navnløs kvinne og en navnløs mann som kjøper seg et hus nede ved sjøen. «Ho» og «Han» ønsker å bo «aleine i kvarandre», men den «tosomme ensomheten» de lever i, blir utfordret da husets tidligere eier involverer seg i livene deres. Den tidligere huseierens nærvær vekker sjalusi, usikkerhet og voldsomme følelser mellom de to hovedkarakterene.
 
-  p
-    em Nokon kjem til å komme
-    | har, i likhet med de fleste Fosse-stykkene, få karakterer. I dette stykket møter vi kun tre karakterer – og disse mangler både navn og særpreg. Jon Fosses karakterer blir ofte framstilt i slike stiliserte roller: Du møter «mor», «far», «son», «mann» og «kvinne». På denne måten blir karakterene representanter for noe allment og typisk, mer enn de representerer noe individuelt. Grunnleggende og allmennmenneskelige følelser og karaktertrekk framstilles slik at et bredt publikum kan finne deler av seg selv – og sine egne liv – i karakterene. Rekvisittene er også ofte få, sceneanvisningene knappe, men viktige. Og karakteristisk for Fosse – vekslingen mellom «kort pause» og «lang pause» gir føringer for hvordan stykket kan spilles.
+  p #[em Nokon kjem til å komme] har, i likhet med de fleste Fosse-stykkene, få karakterer. I dette stykket møter vi kun tre karakterer – og disse mangler både navn og særpreg. Jon Fosses karakterer blir ofte framstilt i slike stiliserte roller: Du møter «mor», «far», «son», «mann» og «kvinne». På denne måten blir karakterene representanter for noe allment og typisk, mer enn de representerer noe individuelt. Grunnleggende og allmennmenneskelige følelser og karaktertrekk framstilles slik at et bredt publikum kan finne deler av seg selv – og sine egne liv – i karakterene. Rekvisittene er også ofte få, sceneanvisningene knappe, men viktige. Og karakteristisk for Fosse – vekslingen mellom «kort pause» og «lang pause» gir føringer for hvordan stykket kan spilles.
 
-  p Som de fleste Jon Fosse-stykker handler også
-    em Nokon kjem til å komme
-    | om relasjoner mellom nære familiemedlemmer eller mellom par. Stykket tematiserer grunnleggende følelsesmessige konflikter som kan oppstå mellom to mennesker som står hverandre nær – følelser som sjalusi, svik og ensomhet. Fosses fokus på par- og familierelasjoner plasserer han i en dominerende trend i litteraturen på 1990- og 2000-tallet: Hvilke vilkår har familien i en postmoderne tid? I Fosses stykker er det ofte avstand, ensomhet og problemer med kommunikasjon som dominerer familielivet.
+  p Som de fleste Jon Fosse-stykker handler også #[em Nokon kjem til å komme] om relasjoner mellom nære familiemedlemmer eller mellom par. Stykket tematiserer grunnleggende følelsesmessige konflikter som kan oppstå mellom to mennesker som står hverandre nær – følelser som sjalusi, svik og ensomhet. Fosses fokus på par- og familierelasjoner plasserer han i en dominerende trend i litteraturen på 1990- og 2000-tallet: Hvilke vilkår har familien i en postmoderne tid? I Fosses stykker er det ofte avstand, ensomhet og problemer med kommunikasjon som dominerer familielivet.
 
-  p
-    em Nokon kjem til å komme
-    | tematiserer på mange måter svært grunnleggende eksistensielle spørsmål: Hvordan forholder vi oss til andre mennesker? Hvordan kommuniserer vi med mennesker rundt oss? Og hvordan kommuniserer vi med oss selv? Denne balansegangen ser vi i
-    em Nokon kjem til å komme som
-    | vekslingen mellom indre monologer og ytre dialoger. De ytre dialogene er replikkene skuespillerne framfører, mens de indre monologene karakterene fører med seg selv, vises i form av pauser og taushet på scenen. Denne tausheten, avstanden mellom menneskene og pausene i kommunikasjonen er på mange måter selve essensen i Fosses forfatterskap.
+  p #[em Nokon kjem til å komme] tematiserer på mange måter svært grunnleggende eksistensielle spørsmål: Hvordan forholder vi oss til andre mennesker? Hvordan kommuniserer vi med mennesker rundt oss? Og hvordan kommuniserer vi med oss selv? Denne balansegangen ser vi i #[em Nokon kjem til å komme] som vekslingen mellom indre monologer og ytre dialoger. De ytre dialogene er replikkene skuespillerne framfører, mens de indre monologene karakterene fører med seg selv, vises i form av pauser og taushet på scenen. Denne tausheten, avstanden mellom menneskene og pausene i kommunikasjonen er på mange måter selve essensen i Fosses forfatterskap.
 
   h3 Språkets utilstrekkelighet
   p Jon Fosse har utviklet en gjenkjennelig språktone der karakterene – enten i romanform eller på scene – dveler ved samme utsagn, ord, dialog eller setning. Disse gjentakelsene får nærmest et musikalsk preg over seg – små forskyvninger i utsagnene fører også til små forskyvninger i mening. Tempoet i tekstene til Fosse blir også uhyre langsomt. Stykkene er preget av lite konkret handling, meningen og innholdet ligger i dialogene – og i pausene.
 
-  p Fosses skrivestil kan minne om den modernistiske
-    em stream of conciousness
-    | -teknikken. Den menneskelige bevisstheten og tankegangen er viktigere å få fram enn faktisk dialog og ytre kommunikasjon. Fosses stykker handler også ofte om stillheten og fraværet av ytre kommunikasjon – det er det indre som er «ekte».
+  p Fosses skrivestil kan minne om den modernistiske #[em stream of consciousness]-teknikken. Den menneskelige bevisstheten og tankegangen er viktigere å få fram enn faktisk dialog og ytre kommunikasjon. Fosses stykker handler også ofte om stillheten og fraværet av ytre kommunikasjon – det er det indre som er «ekte».
 
   p Gjentakelsene og de små forskyvningene i mening tvinger også publikum eller leseren til å reflektere over språket og kommunikasjonens rolle og betydning. Hvor mye mening kan språket egentlig bære? Klarer språket å formidle et menneskes indre tanker og følelser? Kan språket formidle noe sant og virkelig? Dette er problemstillinger kunsten har satt fram på ulike måter etter modernismens gjennombrudd.
 
